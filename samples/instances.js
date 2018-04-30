d6723f1cda427a9371a7e8d0c69fbd09b8593d50
vijay-qlogic <vijay@qlogic.io>
2018-04-30 18:19:40 +0530

    more sample code to instance

--- samples/instances.js
+++ samples/instances.js
@@ -73,7 +73,13 @@ function createInstance() {
     .createInstance('ssd-instance', options)
     .then(results => {
       let instance = results[0];
-      console.log(`Instance created with name ${instance.name}`);
+      let operations = results[1];
+      let apiResponse = results[2];
+
+      operations.on('complete', () => {
+        console.log(`Instance created with name ${instance.name}`);
+        console.log(`apiResponse Object Keys: ${Object.keys(apiResponse)}`);
+      });
     })
     .catch(err => {
       console.error('ERROR:', err);
@@ -115,10 +121,13 @@ function getInstance() {
   // check if instance exists
   instance
     .get()
-    .then(result => {
+    .then(results => {
+      const instance = results[0];
+      // const rawResponse = results[1];
+
       console.log('performed get on ssd-instance:');
-      console.log(`Name: ${result[0].name}`);
-      console.log(`Metadata: ${JSON.stringify(result[0].metadata)}`);
+      console.log(`Name: ${instance.name}`); //result[0] is instance Object
+      console.log(`Metadata: ${JSON.stringify(instance.metadata)}`);
     })
     .catch(err => {
       console.error('ERROR:', err);
@@ -141,6 +150,113 @@ function delInstance() {
     .catch(err => {
       console.error('ERROR:', err);
     });
+}
+
+/**
+ * Create Cluster for ssd-instance
+ */
+function createCluster() {
+  // refer instance with id 'ssd-instance'
+  let instance = bigtable.instance('ssd-instance');
+
+  const callback = (err, cluster, operations, apiResponse) => {
+    if (err) {
+      console.error('ERROR:', err);
+      return;
+    }
+
+    operations.on('error', console.log).on('complete', function() {
+      // The cluster was created successfully.
+      console.log(`Cluster created sucessfully by name: ${cluster.name}`);
+      console.log(`Object keys in apiResponse: ${Object.keys(apiResponse)}`);
+    });
+  };
+
+  let options = {
+    location: 'us-central1-f',
+    nodes: 3,
+    storage: 'ssd',
+  };
+
+  instance.createCluster('my-cluster', options, callback);
+
+  //-
+  // If the callback is omitted, we'll return a Promise.
+  //-
+  // instance.createCluster('my-cluster', options).then(function(data) {
+  //   const cluster = data[0];
+  //   const operation = data[1];
+  //   const apiResponse = data[2];
+  // });
+}
+
+/**
+ * Get Clusters for ssd-instance
+ */
+function listClusters() {
+  // refer instance with id 'ssd-instance'
+  let instance = bigtable.instance('ssd-instance');
+
+  const callback = (err, clusters, apiResponse) => {
+    if (err) {
+      console.error('ERROR:', err);
+      return;
+    }
+    console.log('Clusters List:');
+    clusters.forEach(cluster => {
+      console.log(`${cluster.name} -- ${cluster.metadata.location}`);
+      console.log(`Object keys in apiResponse: ${apiResponse}`);
+    });
+  };
+
+  instance.getClusters(callback);
+}
+
+/**
+ * Create Table for ssd-instance
+ */
+function createTable() {
+  // refer instance with id 'ssd-instance'
+  let instance = bigtable.instance('ssd-instance');
+
+  const callback = (err, table, apiResponse) => {
+    if (err) {
+      console.error('ERROR:', err);
+      return;
+    }
+
+    // The cluster was created successfully.
+    console.log(`Table created sucessfully by name: ${table.name}`);
+    console.log(`Object keys in apiResponse: ${Object.keys(apiResponse)}`);
+  };
+
+  // set optional info
+  let options = {
+    families: ['personal-info', 'business-info'],
+  };
+
+  instance.createTable('my-table2', options, callback);
+}
+
+/**
+ * Get Tables for ssd-instance
+ */
+function listTables() {
+  // refer instance with id 'ssd-instance'
+  let instance = bigtable.instance('ssd-instance');
+
+  const callback = (err, tables) => {
+    if (err) {
+      console.error('ERROR:', err);
+      return;
+    }
+    console.log('Tables List:');
+    tables.forEach(table => {
+      console.log(`${table.name}`);
+    });
+  };
+
+  instance.getTables(callback);
 }
 
 require(`yargs`)
@@ -168,6 +284,22 @@ require(`yargs`)
   // command to delete the instance 'ssd-instance'
   .command(`del-instance`, `Delete the Instance`, {}, delInstance)
   .example(`node $0 del-instance`, `Delete the Instance`)
+
+  // command to create Cluster for instance 'ssd-instance'
+  .command(`create-cluster`, `Creates Cluster`, {}, createCluster)
+  .example(`node $0 create-cluster`, `Create Cluster`)
+
+  // command to list Cluster for instance 'ssd-instance'
+  .command(`list-clusters`, `Lists Clusters`, {}, listClusters)
+  .example(`node $0 list-clusters`, `Lists Clusters`)
+
+  // command to create Table for instance 'ssd-instance'
+  .command(`create-table`, `Creates Table`, {}, createTable)
+  .example(`node $0 create-table`, `Creates Table`)
+
+  // command to list Cluster for instance 'ssd-instance'
+  .command(`list-tables`, `Lists Tables`, {}, listTables)
+  .example(`node $0 list-tables`, `Lists Tables`)
 
   .wrap(120)
   .recommendCommands()
