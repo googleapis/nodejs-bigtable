7e1fd55ec8abb5dd737d3bf251c39efc8c2ea8ad
Chris Wilcox <crwilcox@google.com>
2018-05-03 13:55:22 -0700

    reverting index.js

--- src/v2/index.js
+++ src/v2/index.js
@@ -15,5 +15,9 @@
 'use strict';
 
 const BigtableClient = require('./bigtable_client');
+const BigtableInstanceAdminClient = require('./bigtable_instance_admin_client');
+const BigtableTableAdminClient = require('./bigtable_table_admin_client');
 
 module.exports.BigtableClient = BigtableClient;
+module.exports.BigtableInstanceAdminClient = BigtableInstanceAdminClient;
+module.exports.BigtableTableAdminClient = BigtableTableAdminClient;
