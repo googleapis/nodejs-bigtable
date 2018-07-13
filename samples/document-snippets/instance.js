98c896e803c269c53d845642e597c45468771890
vijay-qlogic <vijay@qlogic.io>
2018-07-13 10:08:09 +0530

    Added Test Cases to check for NO-Error in instance-snippets

--- samples/document-snippets/instance.js
+++ samples/document-snippets/instance.js
@@ -14,369 +14,311 @@
  */
 
 const Bigtable = require('@google-cloud/bigtable');
-const bigtableClient = new Bigtable();
+const bigtable = new Bigtable();
 
-function createInstance(instanceId) {
-  // [START bigtable_create_instance]
-  const instance = bigtableClient.instance(instanceId);
+const snippets = {
+  createInstance: (instanceId, clusterId, callback) => {
+    // [START bigtable_create_instance]
+    const instance = bigtable.instance(instanceId);
 
-  // options for a PRODUCTION Instance
-  const options = {
-    clusters: [
-      {
-        name: 'sample-cluster',
-        nodes: 3,
-        location: 'us-central1-f',
-        storage: 'ssd',
-      },
-    ],
-    type: 'PRODUCTION', // Optional as default type is PRODUCTION
-  };
+    // options for a PRODUCTION Instance
+    // const options = {
+    //   clusters: [
+    //     {
+    //       name: clusterId,
+    //       nodes: 3,
+    //       location: 'us-central1-f',
+    //       storage: 'ssd',
+    //     },
+    //   ],
+    //   type: 'PRODUCTION', // Optional as default type is PRODUCTION
+    // };
 
-  // // options for a DEVELOPMENT Instance
-  // const options = {
-  //   clusters: [
-  //     {
-  //       name: 'sample-cluster',
-  //       location: 'us-central1-f',
-  //       storage: 'hdd',
-  //     },
-  //   ],
-  //   type: 'DEVELOPMENT',
-  // };
+    // options for a DEVELOPMENT Instance
+    const options = {
+      clusters: [
+        {
+          name: clusterId,
+          location: 'us-central1-f',
+          storage: 'hdd',
+        },
+      ],
+      type: 'DEVELOPMENT',
+    };
 
-  // creates a new Instance
-  instance
-    .create(options)
-    .then(result => {
-      let newInstance = result[0];
-      // let operations = result[1];
-      // let apiResponse = result[2];
+    // creates a new Instance
+    instance
+      .create(options)
+      .then(result => {
+        let newInstance = result[0];
+        // let operations = result[1];
+        // let apiResponse = result[2];
 
-      console.log(`Created Instance: ${newInstance.id}`);
-    })
-    .catch(err => {
-      console.error('Error creating prod-instance:', err);
-    });
-  // [END bigtable_create_instance]
-}
-
-function createCluster(instanceId, clusterId) {
-  // [START bigtable_create_cluster]
-  const instance = bigtableClient.instance(instanceId);
-
-  const options = {
-    location: 'us-central1-b',
-    nodes: 3,
-    storage: 'ssd',
-  };
-
-  instance
-    .createCluster(clusterId, options)
-    .then(result => {
-      const newCluster = result[0];
-      // const operations = result[1];
-      // const apiResponse = result[2];
-      console.log(`Cluster created: ${newCluster.id}`);
-    })
-    .catch(err => {
-      console.error('Error creating cluster: ', err);
-    });
-  // [END bigtable_create_cluster]
-}
-
-function createAppProfile(instanceId, clusterId, appProfileId) {
-  // [START bigtable_create_app_profile]
-  const instance = bigtableClient.instance(instanceId);
-  const cluster = instance.cluster(clusterId);
-
-  const options = {
-    routing: cluster,
-    allowTransactionalWrites: true,
-    ignoreWarnings: true,
-  };
-
-  instance.createAppProfile(appProfileId, options, (err, appProfile) => {
-    if (err) {
-      console.error('Error creating appProfile: ', err);
-      return;
-    }
-    console.log(`App-Profile created: ${appProfile.name}`);
-  });
-  // [END bigtable_create_app_profile]
-}
-
-function createTable(instanceId, tableId) {
-  // [START bigtable_create_table]
-  const instance = bigtableClient.instance(instanceId);
-
-  const options = {
-    families: ['follows'],
-  };
-
-  // You can also specify garbage collection rules for your column families.
-  // See {@link Table#createFamily} for more information about
-  // column families and garbage collection rules.
-  //-
-  // const options = {
-  //   families: [
-  //     {
-  //       name: 'follows',
-  //       rule:  {
-  //         age: {
-  //           seconds: 0,
-  //           nanos: 5000
-  //         },
-  //         versions: 3,
-  //         union: true
-  //       }
-  //     }
-  //   ]
-  // };
-
-  instance
-    .createTable(tableId, options)
-    .then(result => {
-      const newTable = result[0];
-      // const apiResponse = result[1];
-      console.log(`Table created: ${newTable.name}`);
-    })
-    .catch(err => {
-      console.error('Error creating table: ', err);
-    });
-  // [END bigtable_create_table]
-}
-
-function existsInstance(instanceId) {
-  const instance = bigtableClient.instance(instanceId);
-
-  // [START bigtable_exists_instance]
-  instance
-    .exists()
-    .then(result => {
-      const exists = result[0];
-      console.log(`Instance ${instanceId} Exists: ${exists}`);
-    })
-    .catch(err => {
-      console.error('Error in checking Instance exists: ', err);
-    });
-  // [END bigtable_exists_instance]
-}
-
-function getInstance(instanceId) {
-  const instance = bigtableClient.instance(instanceId);
-
-  // [START bigtable_get_instance]
-  instance
-    .get()
-    .then(result => {
-      const instance = result[0];
-      // const apiResponse = result[1];
-      console.log(`Instance: \n${instance}`);
-    })
-    .catch(err => {
-      console.error('Error geting Instance: ', err);
-    });
-  // [END bigtable_get_instance]
-}
-
-function getClusters(instanceId) {
-  const instance = bigtableClient.instance(instanceId);
-
-  // [START bigtable_get_clusters]
-  instance
-    .getClusters()
-    .then(result => {
-      console.log(`Clusters: \n${result[0]}`);
-    })
-    .catch(err => {
-      console.error('Error geting Clusters: ', err);
-    });
-  // [END bigtable_get_clusters]
-}
-
-function getAppProfiles(instanceId) {
-  const instance = bigtableClient.instance(instanceId);
-
-  // [START bigtable_get_app_profiles]
-  instance
-    .getAppProfiles()
-    .then(result => {
-      console.log(`AppProfiles: \n${result[0]}`);
-    })
-    .catch(err => {
-      console.error('Error geting AppProfiles: ', err);
-    });
-  // [END bigtable_get_app_profiles]
-}
-
-function getMetaData(instanceId) {
-  const instance = bigtableClient.instance(instanceId);
-
-  // [START bigtable_get_instance_metadata]
-  instance
-    .getMetadata()
-    .then(result => {
-      const metaData = result[0];
-      // const apiResponse = result[1];
-      console.log('%s %O', 'Instance Metadata:\n', metaData);
-    })
-    .catch(err => {
-      console.error('Error geting Metadata: ', err);
-    });
-  // [END bigtable_get_instance_metadata]
-}
-
-function getTables(instanceId) {
-  const instance = bigtableClient.instance(instanceId);
-
-  // [START bigtable_get_tables]
-
-  // To control how many API requests are made and page through the results
-  // manually, set `autoPaginate` to false.
-  const options = {
-    autoPaginate: false,
-  };
-  // const options = {
-  //   autoPaginate: true
-  // };
-
-  instance
-    .getTables(options)
-    .then(result => {
-      console.log(`Tables:`);
-      let tables = result[0];
-      tables.forEach(t => {
-        console.log(t.id);
+        console.log(`Created Instance: ${newInstance.id}`);
+        callback(null, newInstance);
+      })
+      .catch(err => {
+        console.error('Error creating prod-instance:', err);
+        callback(err);
       });
-    })
-    .catch(err => {
-      console.error('Error geting Tables: ', err);
+    // [END bigtable_create_instance]
+  },
+
+  createCluster: (instanceId, clusterId, callback) => {
+    // [START bigtable_create_cluster]
+    const instance = bigtable.instance(instanceId);
+
+    // const options = {
+    //   location: 'us-central1-b',
+    //   nodes: 3,
+    //   storage: 'ssd',
+    // };
+
+    const options = {
+      location: 'us-central1-b',
+      storage: 'hdd',
+    };
+
+    instance
+      .createCluster(clusterId, options)
+      .then(result => {
+        const newCluster = result[0];
+        // const operations = result[1];
+        // const apiResponse = result[2];
+        console.log(`Cluster created: ${newCluster.id}`);
+        callback(null, newCluster);
+      })
+      .catch(err => {
+        console.error('Error creating cluster: ', err);
+        callback(err);
+      });
+    // [END bigtable_create_cluster]
+  },
+
+  createAppProfile: (instanceId, clusterId, appProfileId, callback) => {
+    // [START bigtable_create_app_profile]
+    const instance = bigtable.instance(instanceId);
+    const cluster = instance.cluster(clusterId);
+
+    const options = {
+      routing: cluster,
+      allowTransactionalWrites: true,
+      ignoreWarnings: true,
+    };
+
+    instance.createAppProfile(appProfileId, options, (err, appProfile) => {
+      if (err) {
+        console.error('Error creating appProfile: ', err);
+        callback(err);
+        return;
+      }
+      console.log(`App-Profile created: ${appProfile.name}`);
+      callback(appProfile);
     });
-  // [END bigtable_get_tables]
-}
+    // [END bigtable_create_app_profile]
+  },
 
-function updateInstance(instanceId) {
-  const instance = bigtableClient.instance(instanceId);
+  createTable: (instanceId, tableId, callback) => {
+    // [START bigtable_create_table]
+    const instance = bigtable.instance(instanceId);
 
-  // [START bigtable_set_meta_data]
-  let metadata = {
-    displayName: 'updated-name',
-  };
+    const options = {
+      families: ['follows'],
+    };
 
-  instance
-    .setMetadata(metadata)
-    .then(result => {
-      console.log(`API Response: \n${result[0]}`);
-    })
-    .catch(err => {
-      console.error('Error in Set MetaData: ', err);
-    });
-  // [END bigtable_set_meta_data]
-}
+    // You can also specify garbage collection rules for your column families.
+    // See {@link Table#createFamily} for more information about
+    // column families and garbage collection rules.
+    //-
+    // const options = {
+    //   families: [
+    //     {
+    //       name: 'follows',
+    //       rule:  {
+    //         age: {
+    //           seconds: 0,
+    //           nanos: 5000
+    //         },
+    //         versions: 3,
+    //         union: true
+    //       }
+    //     }
+    //   ]
+    // };
 
-function delInstance(instanceId) {
-  // [START bigtable_del_instance]
-  const instance = bigtableClient.instance(instanceId);
-  instance
-    .delete()
-    .then(result => {
-      const apiResponse = result[0];
-      console.log(`Instance ${instanceId} deleted: ${apiResponse}`);
-    })
-    .catch(err => {
-      console.error('Error deleting instance: ', err);
-    });
-  // [END bigtable_del_instance]
-}
+    instance
+      .createTable(tableId, options)
+      .then(result => {
+        const newTable = result[0];
+        // const apiResponse = result[1];
+        console.log(`Table created: ${newTable.name}`);
+        callback(null, newTable);
+      })
+      .catch(err => {
+        console.error('Error creating table: ', err);
+        callback(err);
+      });
+    // [END bigtable_create_table]
+  },
 
-require(`yargs`)
-  .demand(1)
+  existsInstance: (instanceId, callback) => {
+    const instance = bigtable.instance(instanceId);
 
-  // create Instance
-  .command(`new-instance`, `Creates an Instance`, {}, argv =>
-    createInstance(argv.instance)
-  )
-  .example(`node $0 new-instance --instance [instanceid]`)
+    // [START bigtable_exists_instance]
+    instance
+      .exists()
+      .then(result => {
+        const exists = result[0];
+        console.log(`Instance ${instanceId} Exists: ${exists}`);
+        callback(null, exists);
+      })
+      .catch(err => {
+        console.error('Error in checking Instance exists: ', err);
+        callback(err);
+      });
+    // [END bigtable_exists_instance]
+  },
 
-  // create Cluster
-  .command(`new-cluster`, `Creates a Cluster`, {}, argv =>
-    createCluster(argv.instance, argv.cluster)
-  )
-  .example(`node $0 new-cluster --cluster [clusterId] --instance [instanceid]`)
+  getInstance: (instanceId, callback) => {
+    const instance = bigtable.instance(instanceId);
 
-  // create App-Profile
-  .command(`new-app-profile`, `Creates an AppProfile`, {}, argv =>
-    createAppProfile(argv.instance, argv.cluster, argv.appProfile)
-  )
-  .example(
-    `node $0 new-app-profile --appProfile [appProfileId]--cluster [clusterId] --instance [instanceId]`
-  )
+    // [START bigtable_get_instance]
+    instance
+      .get()
+      .then(result => {
+        const instance = result[0];
+        // const apiResponse = result[1];
+        console.log(`Instance: \n${instance}`);
+        callback(null, instance);
+      })
+      .catch(err => {
+        console.error('Error geting Instance: ', err);
+        callback(err);
+      });
+    // [END bigtable_get_instance]
+  },
 
-  // create Table
-  .command(`new-table`, `Creates a Table`, {}, argv =>
-    createTable(argv.instance, argv.table)
-  )
-  .example(`node $0 new-table --table [tableId] --instance [instanceId]`)
+  getClusters: (instanceId, callback) => {
+    const instance = bigtable.instance(instanceId);
 
-  // check Instance exists
-  .command(`exist-instance`, `Check Instance Exists`, {}, argv =>
-    existsInstance(argv.instance)
-  )
-  .example(`node $0 exist-instance --instance [instanceid]`)
+    // [START bigtable_get_clusters]
+    instance
+      .getClusters()
+      .then(result => {
+        console.log(`Clusters: \n${result[0]}`);
+        callback(null, result);
+      })
+      .catch(err => {
+        console.error('Error geting Clusters: ', err);
+        callback(err);
+      });
+    // [END bigtable_get_clusters]
+  },
 
-  // get an existing Instance
-  .command(`get-instance`, `Get an existing Instance`, {}, argv =>
-    getInstance(argv.instance)
-  )
-  .example(`node $0 get-instance --instance [instanceid]`)
+  getAppProfiles: (instanceId, callback) => {
+    const instance = bigtable.instance(instanceId);
 
-  // get Clusters
-  .command(`get-clusters`, `Get Clusters`, {}, argv =>
-    getClusters(argv.instance)
-  )
-  .example(`node $0 get-clusters --instance [instanceid]`)
+    // [START bigtable_get_app_profiles]
+    instance
+      .getAppProfiles()
+      .then(result => {
+        console.log(`AppProfiles: \n${result[0]}`);
+        callback(null, result);
+      })
+      .catch(err => {
+        console.error('Error geting AppProfiles: ', err);
+        callback(err);
+      });
+    // [END bigtable_get_app_profiles]
+  },
 
-  // get AppProfiles
-  .command(`get-app-profiles`, `Get AppProfiles`, {}, argv =>
-    getAppProfiles(argv.instance)
-  )
-  .example(`node $0 get-app-profiles --instance [instanceid]`)
+  getMetaData: (instanceId, callback) => {
+    const instance = bigtable.instance(instanceId);
 
-  // get Instance Metadata
-  .command(`get-imeta`, `Get Instance Metadata`, {}, argv =>
-    getMetaData(argv.instance)
-  )
-  .example(`node $0 get-imeta --instance [instanceid]`)
+    // [START bigtable_get_instance_metadata]
+    instance
+      .getMetadata()
+      .then(result => {
+        const metaData = result[0];
+        // const apiResponse = result[1];
+        console.log('%s %O', 'Instance Metadata:\n', metaData);
+        callback(null, metaData);
+      })
+      .catch(err => {
+        console.error('Error geting Metadata: ', err);
+        callback(err);
+      });
+    // [END bigtable_get_instance_metadata]
+  },
 
-  // get Instance Tables
-  .command(`get-tables`, `Get Instance Tables`, {}, argv =>
-    getTables(argv.instance)
-  )
-  .example(`node $0 get-tables --instance [instanceid]`)
+  getTables: (instanceId, callback) => {
+    const instance = bigtable.instance(instanceId);
 
-  // set metaData for Instance
-  .command(`update-instance`, `Set Instance MetaData`, {}, argv =>
-    updateInstance(argv.instance)
-  )
-  .example(`node $0 update-instance --instance [instanceid]`)
+    // [START bigtable_get_tables]
 
-  // delete Instance
-  .command(`del-instance`, `Delete an Instance`, {}, argv =>
-    delInstance(argv.instance)
-  )
+    // To control how many API requests are made and page through the results
+    // manually, set `autoPaginate` to false.
+    const options = {
+      autoPaginate: false,
+    };
+    // const options = {
+    //   autoPaginate: true
+    // };
 
-  .example(`node $0 del-instance --instance [instanceid]`)
-  .wrap(120)
-  .nargs('instance', 1)
-  .describe('instance', 'Cloud Bigtable Instance ID')
-  .nargs('cluster', 1)
-  .describe('cluster', 'Cloud Bigtable Cluster ID')
-  .nargs('appProfile', 1)
-  .describe('appProfile', 'Cloud Bigtable App Profile ID')
-  .nargs('table', 1)
-  .describe('table', 'Cloud Bigtable Table ID')
-  .recommendCommands()
-  .epilogue(`For more information, see https://cloud.google.com/bigtable/docs`)
-  .help()
-  .strict().argv;
+    instance
+      .getTables(options)
+      .then(result => {
+        console.log(`Tables:`);
+        let tables = result[0];
+        tables.forEach(t => {
+          console.log(t.id);
+        });
+        callback(null, tables);
+      })
+      .catch(err => {
+        console.error('Error geting Tables: ', err);
+        callback(err);
+      });
+    // [END bigtable_get_tables]
+  },
+
+  updateInstance: (instanceId, callback) => {
+    const instance = bigtable.instance(instanceId);
+
+    // [START bigtable_set_meta_data]
+    let metadata = {
+      displayName: 'updated-name',
+    };
+
+    instance
+      .setMetadata(metadata)
+      .then(result => {
+        console.log(`API Response: \n${result[0]}`);
+        callback(null, result);
+      })
+      .catch(err => {
+        console.error('Error in Set MetaData: ', err);
+        callback(err);
+      });
+    // [END bigtable_set_meta_data]
+  },
+
+  delInstance: (instanceId, callback) => {
+    // [START bigtable_del_instance]
+    const instance = bigtable.instance(instanceId);
+    instance
+      .delete()
+      .then(result => {
+        const apiResponse = result[0];
+        console.log(`Instance ${instanceId} deleted: ${apiResponse}`);
+        callback(null, apiResponse);
+      })
+      .catch(err => {
+        console.error('Error deleting instance: ', err);
+        callback(err);
+      });
+    // [END bigtable_del_instance]
+  },
+};
+
+module.exports = snippets;
