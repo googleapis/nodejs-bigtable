9c5bf237d145b96fecdb1f535df123e1824ec0d3
vijay-qlogic <vijay@qlogic.io>
2018-08-06 01:04:16 +0530

    Added example region-tags for row object

---
+++ samples/document-snippets/row.js
@@ -0,0 +1,265 @@
+/**
+ * Copyright 2018, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const Bigtable = require('@google-cloud/bigtable');
+const bigtable = new Bigtable();
+
+const snippets = {
+  createRow: (instanceId, tableId, callback) => {
+    const instance = bigtable.instance(instanceId);
+    const table = instance.table(tableId);
+
+    // [START bigtable_create_row]
+    const row = table.row('samplerow');
+
+    row
+      .create()
+      .then(result => {
+        let apiResponse = result[0];
+      })
+      .catch(err => {
+        // handle error
+      });
+    // [END bigtable_create_row]
+
+    callback();
+  },
+
+  createRules: (instanceId, tableId, callback) => {
+    const instance = bigtable.instance(instanceId);
+    const table = instance.table(tableId);
+
+    // [START bigtable_create_rules]
+    const row = table.row('samplerow');
+    var rules = [
+      {
+        column: 'follows:alincoln',
+        append: ' Honest Abe!',
+      },
+    ];
+    row
+      .createRules(rules)
+      .then(result => {
+        let apiResponse = result[0];
+      })
+      .catch(err => {
+        // handle error
+      });
+    // [END bigtable_create_rules]
+
+    callback();
+  },
+
+  deleteAllCells: (instanceId, tableId, callback) => {
+    const instance = bigtable.instance(instanceId);
+    const table = instance.table(tableId);
+
+    // [START bigtable_delete_all_cells]
+    const row = table.row('samplerow');
+    row
+      .delete()
+      .then(function(data) {
+        let apiResponse = data[0];
+      })
+      .catch(err => {
+        // handle error
+      });
+    // [END bigtable_delete_all_cells]
+
+    callback();
+  },
+
+  deleteCells: (instanceId, tableId, callback) => {
+    const instance = bigtable.instance(instanceId);
+    const table = instance.table(tableId);
+
+    // [START bigtable_delete_particular_cells]
+    const row = table.row('samplerow');
+
+    // Delete all cells within a family.
+    let cells = ['follows'];
+    row
+      .deleteCells(cells)
+      .then(result => {
+        let apiResponse = result[0];
+      })
+      .catch(err => {
+        // handle error
+      });
+    // [END bigtable_delete_particular_cells]
+
+    callback();
+  },
+
+  exists: (instanceId, tableId, callback) => {
+    const instance = bigtable.instance(instanceId);
+    const table = instance.table(tableId);
+
+    // [START bigtable_row_exists]
+    const row = table.row('samplerow');
+
+    row
+      .exists()
+      .then(result => {
+        let exists = result[0];
+      })
+      .catch(err => {
+        // handle error
+      });
+    // [END bigtable_row_exists]
+
+    callback();
+  },
+
+  filter: (instanceId, tableId, callback) => {
+    const instance = bigtable.instance(instanceId);
+    const table = instance.table(tableId);
+
+    // [START bigtable_row_filter]
+    const row = table.row('samplerow');
+
+    let filter = [
+      {
+        family: 'follows',
+      },
+      {
+        column: 'alincoln',
+      },
+      {
+        value: 1,
+      },
+    ];
+
+    // Optionally, you can pass in an array of entries to be ran in the event
+    // that a match is not made.
+    let config = {
+      onNoMatch: [
+        {
+          method: 'insert',
+          data: {
+            follows: {
+              jadams: 1,
+            },
+          },
+        },
+      ],
+    };
+
+    row
+      .filter(filter, config)
+      .then(data => {
+        var matched = data[0];
+      })
+      .catch(err => {
+        // handle error
+      });
+    // [END bigtable_row_filter]
+
+    callback();
+  },
+
+  get: (instanceId, tableId, callback) => {
+    const instance = bigtable.instance(instanceId);
+    const table = instance.table(tableId);
+
+    // [START bigtable_get_row]
+    const row = table.row('samplerow');
+
+    row
+      .get()
+      .then(result => {
+        let row = result[0];
+      })
+      .catch(err => {
+        // handle error
+      });
+    // [END bigtable_get_row]
+
+    callback();
+  },
+
+  getMetadata: (instanceId, tableId, callback) => {
+    const instance = bigtable.instance(instanceId);
+    const table = instance.table(tableId);
+
+    // [START bigtable_get_row_meta]
+    const row = table.row('samplerow');
+
+    row
+      .getMetadata()
+      .then(result => {
+        let metaData = result[0];
+        let apiResponse = result[1];
+      })
+      .catch(err => {
+        // handle error
+      });
+    // [END bigtable_get_row_meta]
+
+    callback();
+  },
+
+  increment: (instanceId, tableId, callback) => {
+    const instance = bigtable.instance(instanceId);
+    const table = instance.table(tableId);
+
+    // [START bigtable_row_increment]
+    const row = table.row('samplerow');
+
+    // Specify a custom amount to increment the column by.
+    // row.increment('follows:gwashington', 2);
+
+    // To decrement a column, simply supply a negative value.
+    // row.increment('follows:gwashington', -1);
+    row
+      .increment('follows:gwashington')
+      .then(result => {
+        let value = result[0];
+        let apiResponse = result[1];
+      })
+      .catch(err => {
+        // handle error
+      });
+    // [END bigtable_row_increment]
+
+    callback();
+  },
+
+  save: (instanceId, tableId, callback) => {
+    const instance = bigtable.instance(instanceId);
+    const table = instance.table(tableId);
+
+    // [START bigtable_row_save]
+    const row = table.row('samplerow');
+    let entry = {
+      follows: {
+        jadams: 1,
+      },
+    };
+    row
+      .save(entry)
+      .then(result => {
+        let apiResponse = result[0];
+      })
+      .catch(err => {
+        // handle error
+      });
+    // [END bigtable_row_save]
+
+    callback();
+  },
+};
+
+module.exports = snippets;
---
+++ samples/document-snippets/tests/row.js
@@ -0,0 +1,79 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+const assert = require('assert');
+const uuid = require(`uuid`);
+
+const Bigtable = require(`@google-cloud/bigtable`);
+const bigtable = new Bigtable();
+
+const INSTANCE_ID = `nodejs-bigtable-samples-${uuid.v4()}`.substr(0, 30); // Bigtable naming rules
+const CLUSTER_ID = `nodejs-bigtable-samples-${uuid.v4()}`.substr(0, 30); // Bigtable naming rules
+const TABLE_ID = `nodejs-bigtable-samples-${uuid.v4()}`.substr(0, 30); // Bigtable naming rules
+// const FAMILY_ID = `sample-family-${uuid.v4()}`.substr(0, 10); // Bigtable naming rules
+
+const rowSnippets = require('../row.js');
+
+const instance = bigtable.instance(INSTANCE_ID);
+
+describe.only('Row Snippets', function() {
+  before(async () => {
+    await instance.create({
+      clusters: [
+        {
+          name: CLUSTER_ID,
+          location: 'us-central1-f',
+          storage: 'hdd',
+        },
+      ],
+      type: 'DEVELOPMENT',
+    });
+    await instance.createTable(TABLE_ID);
+  });
+
+  after(async () => {
+    await instance.delete();
+  });
+
+  it('should create a row', done => {
+    rowSnippets.createRow(INSTANCE_ID, TABLE_ID, done);
+  });
+  it('should create a row rules', done => {
+    rowSnippets.createRules(INSTANCE_ID, TABLE_ID, done);
+  });
+  it('should delete all cells', done => {
+    rowSnippets.deleteAllCells(INSTANCE_ID, TABLE_ID, done);
+  });
+  it('should delete selected cells', done => {
+    rowSnippets.deleteCells(INSTANCE_ID, TABLE_ID, done);
+  });
+  it('should check row exists', done => {
+    rowSnippets.exists(INSTANCE_ID, TABLE_ID, done);
+  });
+  it('should mutate row with matched filter', done => {
+    rowSnippets.filter(INSTANCE_ID, TABLE_ID, done);
+  });
+  it('should get row meta-data', done => {
+    rowSnippets.getMetadata(INSTANCE_ID, TABLE_ID, done);
+  });
+  it('should increment row', done => {
+    rowSnippets.increment(INSTANCE_ID, TABLE_ID, done);
+  });
+  it('should save row', done => {
+    rowSnippets.save(INSTANCE_ID, TABLE_ID, done);
+  });
+});
