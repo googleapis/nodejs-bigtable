2515d873066b3697a310f52cb1b017d3fe06ffa9
vijay-qlogic <vijay@qlogic.io>
2018-08-29 11:41:30 +0530

    Added example region-tags for app-profile object

---
+++ samples/document-snippets/app-profile.js
@@ -0,0 +1,128 @@
+/**
+ * Copyright 2018, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// const Bigtable = require('@google-cloud/bigtable');
+const Bigtable = require('../../src');
+const bigtable = new Bigtable();
+
+const snippets = {
+  create: (instanceId, appProfileId) => {
+    // [START bigtable_create_app_profile]
+    const instance = bigtable.instance(instanceId);
+    const appProfile = instance.appProfile(appProfileId);
+
+    appProfile
+      .create()
+      .then(data => {
+        const appProfile = data[0];
+        const apiResponse = data[1];
+      })
+      .catch(err => {
+        // Handle the error.
+      });
+    // [END bigtable_create_app_profile]
+  },
+
+  delete: (instanceId, appProfileId) => {
+    // [START bigtable_delete_app_profile]
+    const instance = bigtable.instance(instanceId);
+    const appProfile = instance.appProfile(appProfileId);
+
+    appProfile
+      .delete()
+      .then(data => {
+        const apiResponse = data[0];
+      })
+      .catch(err => {
+        // Handle the error.
+      });
+    // [END bigtable_delete_app_profile]
+  },
+
+  exists: (instanceId, appProfileId) => {
+    // [START bigtable_exists_app_profile]
+    const instance = bigtable.instance(instanceId);
+    const appProfile = instance.appProfile(appProfileId);
+
+    appProfile
+      .exists()
+      .then(data => {
+        const exists = data[0];
+      })
+      .catch(err => {
+        // Handle the error.
+      });
+    // [END bigtable_exists_app_profile]
+  },
+
+  get: (instanceId, appProfileId) => {
+    // [START bigtable_get_app_profile]
+    const instance = bigtable.instance(instanceId);
+    const appProfile = instance.appProfile(appProfileId);
+
+    appProfile
+      .get()
+      .then(data => {
+        const appProfile = data[0];
+        const apiResponse = data[1];
+      })
+      .catch(err => {
+        // Handle the error.
+      });
+    // [END bigtable_get_app_profile]
+  },
+
+  getMeta: (instanceId, appProfileId) => {
+    // [START bigtable_app_profile_get_meta]
+    const instance = bigtable.instance(instanceId);
+    const appProfile = instance.appProfile(appProfileId);
+
+    appProfile
+      .getMetadata()
+      .then(data => {
+        const metadata = data[0];
+        const apiResponse = data[1];
+      })
+      .catch(err => {
+        // Handle the error.
+      });
+    // [END bigtable_app_profile_get_meta]
+  },
+
+  setMeta: (instanceId, appProfileId, clusterId) => {
+    // [START bigtable_app_profile_set_meta]
+    const instance = bigtable.instance(instanceId);
+    const cluster = instance.cluster(clusterId);
+    const appProfile = instance.appProfile(appProfileId);
+
+    const metadata = {
+      description: 'My Updated App Profile',
+      routing: cluster,
+      allowTransactionalWrites: true,
+    };
+
+    appProfile
+      .setMetadata(metadata)
+      .then(data => {
+        const apiResponse = data[0];
+      })
+      .catch(err => {
+        // Handle the error.
+      });
+    // [END bigtable_app_profile_set_meta]
+  },
+};
+
+module.exports = snippets;
---
+++ samples/document-snippets/tests/app-profile.js
@@ -0,0 +1,77 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+const uuid = require(`uuid`);
+
+const Bigtable = require(`@google-cloud/bigtable`);
+const bigtable = new Bigtable();
+
+const INSTANCE_ID = `nodejs-bigtable-samples-${uuid.v4()}`.substr(0, 30); // Bigtable naming rules
+const CLUSTER_ID = `nodejs-bigtable-samples-${uuid.v4()}`.substr(0, 30); // Bigtable naming rules
+const APP_PROFILE_ID = `my-app-profile`;
+
+const appProfileSnippets = require('../app-profile.js');
+
+const instance = bigtable.instance(INSTANCE_ID);
+
+describe('App Profile Snippets', function() {
+  before(() => {
+    instance.create({
+      clusters: [
+        {
+          name: CLUSTER_ID,
+          location: 'us-central1-f',
+          storage: 'hdd',
+        },
+      ],
+      type: 'DEVELOPMENT',
+    });
+  });
+
+  after(() => {
+    instance.exists().then(result => {
+      const exists = result[0];
+      if (exists) {
+        instance.delete();
+      }
+    });
+  });
+
+  it('should create an app-profile', () => {
+    appProfileSnippets.create(INSTANCE_ID, APP_PROFILE_ID);
+  });
+
+  it('should check app-profile exists', () => {
+    appProfileSnippets.exists(INSTANCE_ID, APP_PROFILE_ID);
+  });
+
+  it('should get the app-profile', () => {
+    appProfileSnippets.get(INSTANCE_ID, APP_PROFILE_ID);
+  });
+
+  it('should get app-profile metadata', () => {
+    appProfileSnippets.getMeta(INSTANCE_ID, APP_PROFILE_ID);
+  });
+
+  it('should set app-profile metadata', () => {
+    appProfileSnippets.setMeta(INSTANCE_ID, APP_PROFILE_ID, CLUSTER_ID);
+  });
+
+  it('should delete an app-profile', () => {
+    appProfileSnippets.delete(INSTANCE_ID, APP_PROFILE_ID);
+  });
+});
