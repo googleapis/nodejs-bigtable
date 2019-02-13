a0183ddead24a42838e48867a66630bb7fcd2380
yoshi-automation <yoshi-automation@google.com>
2019-02-13 04:10:12 -0800

    [CHANGE ME] Re-generated  to pick up changes in the API or client library generator.

--- src/v2/bigtable_client.js
+++ src/v2/bigtable_client.js
@@ -150,6 +150,10 @@ class BigtableClient {
             function() {
               const args = Array.prototype.slice.call(arguments, 0);
               return stub[methodName].apply(stub, args);
+            },
+          err =>
+            function() {
+              throw err;
             }
         ),
         defaults[methodName],
