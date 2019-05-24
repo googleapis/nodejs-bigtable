0e94d47069dd792263f43ef3c53900812d4a65af
Billy Jacobson <billy.jacobson@gmail.com>
2019-05-24 17:32:01 -0400

    Write samples

---
+++ samples/writeConditionally.js
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2019, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+function main(instanceId = 'YOUR_INSTANCE_ID') {
+  // [START bigtable_writes_conditoinal]
+  /**
+   * TODO(developer): Uncomment these variables before running the sample.
+   */
+  // const instanceId = 'YOUR_INSTANCE_ID';
+
+  const Bigtable = require('@google-cloud/bigtable');
+
+  const bigtable = Bigtable();
+
+  async function writeConditionally() {
+    const instance = bigtable.instance(instanceId);
+    const table = instance.table('mobile-time-series');
+
+    const row = table.row('phone#4c410523#20190501');
+    const filter = [
+      {
+        column: 'os_build',
+        value: {
+          start: 'PQ2A',
+          end: 'PQ2A',
+        },
+      },
+    ];
+
+    const config = {
+      onMatch: [
+        {
+          method: 'insert',
+          data: {
+            stats_summary: {
+              os_name: 'android',
+            },
+          },
+        },
+      ],
+    };
+
+    await row.filter(filter, config);
+
+    console.log(`Successfully updated row's os_name`);
+  }
+
+  writeConditionally();
+  // [END bigtable_writes_conditoinal]
+}
+
+main(...process.argv.slice(2));
