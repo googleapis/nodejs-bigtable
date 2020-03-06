733197929690c82a5e019470a7938794e2154490
xiaozhenliugg <xiaozhenliu@google.com>
2020-03-06 13:35:33 -0800

    add timeout to 10min for droprange

--- synth.py
+++ synth.py
@@ -5,46 +5,51 @@ import subprocess
 import os
 
 logging.basicConfig(level=logging.DEBUG)
-
-gapic = gcp.GAPICGenerator()
-v2_library = gapic.node_library(
+version='v2'
+gapic = gcp.GAPICMicrogenerator()
+v2_library = gapic.typescript_library(
   'bigtable',
   'v2',
-  config_path='/google/bigtable/artman_bigtable.yaml'
-)
+  generator_args={
+    "grpc-service-config": f"google/bigtable/{version}/bigtable_grpc_service_config.json",
+    "package-name": f"@google-cloud/bigtable",
+    "main-service": f"bigtable"
+    },
+    proto_path=f'/google/bigtable/{version}',
+    extra_proto_files=['google/cloud/common_resources.proto'],
+  )
 
 s.copy(
   v2_library,
-  excludes=['package.json', 'README.md', 'src/index.js', 'src/v2/index.js']
+  excludes=['package.json', 'README.md', 'src/index.ts', 'src/v2/index.ts']
 )
 
-v2_library = gapic.node_library(
+v2_library = gapic.typescript_library(
     "bigtable_admin",
     "v2",
-    config_path="/google/bigtable/admin/artman_bigtableadmin.yaml",
-    artman_output_name="bigtable-admin-v2"
-)
-
+    generator_args={
+      "grpc-service-config": f"google/bigtable/admin/{version}/bigtableadmin_grpc_service_config.json",
+      "main-service": f"bigtable"
+      },
+      proto_path=f'/google/bigtable/admin/{version}',
+      extra_proto_files=['google/cloud/common_resources.proto'],
+      )
 s.copy(
   v2_library,
-  excludes=['package.json', 'README.md', 'src/index.js', 'src/v2/index.js']
+  excludes=['package.json', 'README.md', 'src/index.ts', 'src/v2/index.ts']
 )
 
 # Update path discovery due to build/ dir and TypeScript conversion.
-s.replace("src/v2/bigtable_client.js", "../../package.json", "../../../package.json")
-s.replace("src/v2/bigtable_instance_admin_client.js", "../../package.json", "../../../package.json")
-s.replace("src/v2/bigtable_table_admin_client.js", "../../package.json", "../../../package.json")
+# s.replace("src/v2/bigtable_client.js", "../../package.json", "../../../package.json")
+# s.replace("src/v2/bigtable_instance_admin_client.js", "../../package.json", "../../../package.json")
+# s.replace("src/v2/bigtable_table_admin_client.js", "../../package.json", "../../../package.json")
 
 
 common_templates = gcp.CommonTemplates()
 templates = common_templates.node_library(
   source_location='build/src'
 )
-s.copy(templates, excludes=['.jsdoc.js'])
-
-# No webpack support for TypeScript libraries yet
-os.unlink("webpack.config.js")
-os.unlink("src/browser.js")
+s.copy(templates)
 
 subprocess.run(['npm', 'install'])
 subprocess.run(['npm', 'run', 'fix'])
