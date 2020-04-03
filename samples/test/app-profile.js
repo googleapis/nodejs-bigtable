e183c4b2fbbb35f89314143dd6471c6b6843cb0e
yoshi-automation <yoshi-automation@google.com>
2020-04-03 04:21:40 -0700

    test: reap old instances before system tests (#687)
    https://github.com/googleapis/nodejs-bigtable/commit/7bcae14867e3e07c09d9e93f2b186e6c0fcb5050
    commit 7bcae14867e3e07c09d9e93f2b186e6c0fcb5050
    Author: Justin Beckwith <justin.beckwith@gmail.com>
    Date:   Thu Apr 2 17:16:31 2020 -0700
    
        test: reap old instances before system tests (#687)

--- samples/test/app-profile.js
+++ samples/test/app-profile.js
@@ -19,8 +19,8 @@ const {describe, it, before, after} = require('mocha');
 const Bigtable = require('@google-cloud/bigtable');
 const bigtable = new Bigtable();
 
-const INSTANCE_ID = `nodejs-bigtable-samples-${uuid.v4()}`.substr(0, 30); // Bigtable naming rules
-const CLUSTER_ID = `nodejs-bigtable-samples-${uuid.v4()}`.substr(0, 30); // Bigtable naming rules
+const INSTANCE_ID = `gcloud-tests-${uuid.v4()}`.substr(0, 30); // Bigtable naming rules
+const CLUSTER_ID = `gcloud-tests-${uuid.v4()}`.substr(0, 30); // Bigtable naming rules
 const APP_PROFILE_ID = 'my-app-profile';
 
 const appProfileSnippets = require('./app-profile.js');
