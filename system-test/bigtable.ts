8452fa41f7f11408b8d69fe15f9b6d79c4f46294
bencoe@google.com <bencoe@google.com>
2022-01-12 18:55:50 +0000

    fix: address types regression

--- system-test/bigtable.ts
+++ system-test/bigtable.ts
@@ -276,9 +276,7 @@ describe('Bigtable', () => {
         await operation.promise();
         throw new Error('Cluster creation should not have succeeded');
       } catch (e) {
-        assert(
-          e.message.includes('default keys and CMEKs are not allowed')
-        );
+        assert(e.message.includes('default keys and CMEKs are not allowed'));
       }
     });
   });
