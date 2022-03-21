a7e96b24574fa01e1b5b71450950aaff8524dfa2
Daniel Bruce <djbruce@google.com>
2022-03-21 15:30:04 -0400

    Added ES-lint rule to get linter to pass

--- src/index.ts
+++ src/index.ts
@@ -948,6 +948,7 @@ promisifyAll(Bigtable, {
 
 // Allow creating a `Bigtable` instance without using the `new` keyword.
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
+// eslint-disable-next-line no-class-assign
 (Bigtable as any) = new Proxy(Bigtable, {
   apply(target, thisArg, argumentsList) {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
