e7c83038c73af99173eb0560312097cc56093161
Daniel Bruce <djbruce@google.com>
2022-04-26 13:51:48 -0400

    Add integration test for delete code snippets

--- samples/deleteSnippets.js
+++ samples/deleteSnippets.js
@@ -1,3 +1,17 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 'use strict';
 
 async function main(
@@ -24,12 +38,14 @@ async function main(
       // [START bigtable_deletes_from_row]
       const row = table.row('phone#4c410523#20190501');
       await row.deleteCells(['cell_plan:data_plan_05gb']);
+      await printRows();
       // [END bigtable_deletes_from_row]
       break;
     }
     case 'dropRowRange': {
       // [START bigtable_drop_row_range]
       await table.deleteRows('phone#4c');
+      await printRows();
       // [END bigtable_drop_row_range]
       break;
     }
@@ -37,17 +53,44 @@ async function main(
       // [START bigtable_delete_column_family]
       const cf = table.family('stats_summary');
       await cf.delete({});
+      await printRows();
       // [END bigtable_delete_column_family]
       break;
     }
     case 'deleteTable': {
       // [START bigtable_delete_table]
       await table.delete({});
+      console.log(await table.exists({}));
       // [END bigtable_delete_table]
       break;
     }
   }
   // [END_EXCLUDE]
+
+  async function printRows() {
+    const rows = (await table.getRows({}))[0];
+    for (const row of rows) {
+      const rowData = row.data;
+      for (const columnFamily of Object.keys(rowData)) {
+        const columnFamilyData = rowData[columnFamily];
+        console.log(`Column Family ${columnFamily}`);
+
+        for (const columnQualifier of Object.keys(columnFamilyData)) {
+          const col = columnFamilyData[columnQualifier];
+
+          for (const cell of col) {
+            const labels = cell.labels.length
+              ? ` [${cell.labels.join(',')}]`
+              : '';
+            console.log(
+              `\t${columnQualifier}: ${cell.value} @${cell.timestamp}${labels}`
+            );
+          }
+        }
+      }
+    }
+    console.log();
+  }
 }
 // [END bigtable_deletes_print]
 
