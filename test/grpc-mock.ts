eb94439115d774f3e38918fbf4b52e839a4b6f24
Daniel Bruce <djbruce@google.com>
2022-05-18 17:42:58 -0400

    test for error sent through gax

--- test/grpc-mock.ts
+++ test/grpc-mock.ts
@@ -2,12 +2,13 @@ import {describe, it} from 'mocha';
 import {Bigtable} from '../src';
 import * as v2 from '../src/v2';
 import {PassThrough} from 'stream';
+import * as assert from 'assert';
 
-import * as protos from '../protos/protos';
 import jsonProtos = require('../protos/protos.json');
 
 import grpc = require('@grpc/grpc-js');
 import protoLoader = require('@grpc/proto-loader');
+import {GoogleError} from 'google-gax';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 const packageDefinition = protoLoader.fromJSON(jsonProtos, {
@@ -17,8 +18,26 @@ const packageDefinition = protoLoader.fromJSON(jsonProtos, {
   defaults: true,
   oneofs: true,
 });
-const readRows = (arg1: any, arg2: any) => {
-  console.log(arg1, arg2);
+const errorDetails =
+  'Table not found: projects/my-project/instances/my-instance/tables/my-table';
+const readRows = (call: any, example: any) => {
+  // const internalRepr = new Map();
+  // internalRepr.set('grpc-server-stats-bin', [
+  //   Buffer.from([0, 0, 201, 39, 110, 3, 0, 0, 0, 0]),
+  // ]);
+  // call.emit('error', {
+  //   code: 5,
+  //   details:
+  //     'Table not found: projects/my-project/instances/my-instance/tables/my-table',
+  //   metadata: {
+  //     internalRepr,
+  //     options: {},
+  //   },
+  // });
+  call.emit('error', {
+    code: 5,
+    details: errorDetails,
+  });
 };
 const proto = grpc.loadPackageDefinition(packageDefinition);
 const server = new grpc.Server();
@@ -37,7 +56,7 @@ server.bindAsync(
 );
 
 describe('Bigtable/Grpc-mock', () => {
-  it('error should not change when emitted from gax', done => {
+  it('should produce human readable error when passing through gax', done => {
     const bigtable = new Bigtable({apiEndpoint: 'localhost:1234'});
     const clientConfig = 'BigtableClient';
     const gaxClient = new v2[clientConfig]({});
@@ -47,10 +66,15 @@ describe('Bigtable/Grpc-mock', () => {
     };
     const table = bigtable.instance('fake-instance').table('fake-table');
     const readStream = table.createReadStream({});
-    readStream.on('error', err => {
-      console.log(err);
+    readStream.on('error', (err: GoogleError) => {
+      const {code, statusDetails, message} = err;
+      assert.strictEqual(statusDetails, errorDetails);
+      assert.strictEqual(code, 5);
+      assert.strictEqual(
+        message,
+        '5 NOT_FOUND: Table not found: projects/my-project/instances/my-instance/tables/my-table'
+      );
       done();
     });
-    console.log('test');
   });
 });
