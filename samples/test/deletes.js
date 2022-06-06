57f2a8044005bcf243916ada25ebbfd3a170765a
Daniel Bruce <djbruce@google.com>
2022-06-06 13:39:38 -0400

    Revert "Removed line of code that is shortening timestamp"
    This reverts commit 96adcdbee42e26e53aa270a43713732342cf902c.

--- samples/test/deletes.js
+++ samples/test/deletes.js
@@ -27,6 +27,7 @@ describe('deletes', async () => {
   let table;
   let INSTANCE_ID;
   const TIMESTAMP = new Date(2019, 5, 1);
+  TIMESTAMP.setUTCHours(0);
 
   before(async () => {
     const instance = await obtainTestInstance();
