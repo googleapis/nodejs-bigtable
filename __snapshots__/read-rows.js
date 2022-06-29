32b3da583f9fc94a74a91d2ce9d96daa96b6849b
Daniel Bruce <djbruce@google.com>
2022-06-29 10:04:22 -0400

    new snapshots

--- __snapshots__/read-rows.js
+++ __snapshots__/read-rows.js
@@ -1,71 +1,147 @@
 exports[
   'Bigtable/ReadRows with a mock server that always sends an error back where the error is retryable should ensure correct behavior with deadline exceeded error 1'
 ] = {
-  code: 4,
-  callCount: 4,
-  request: {
-    tableName:
-      'projects/{{projectId}}/instances/fake-instance/tables/fake-table',
-    rows: {
-      rowKeys: [],
-      rowRanges: [{}],
+  result: 'error',
+  output: {
+    request: {
+      tableName:
+        'projects/{{projectId}}/instances/fake-instance/tables/fake-table',
+      rows: {
+        rowKeys: [],
+        rowRanges: [{}],
+      },
+      filter: null,
+      rowsLimit: '0',
+      appProfileId: '',
     },
-    filter: null,
-    rowsLimit: '0',
-    appProfileId: '',
+    code: 4,
+    callCount: 4,
   },
 };
 
 exports[
   'Bigtable/ReadRows with a mock server that always sends an error back where the error is retryable should ensure correct behavior with resource exhausted error 1'
 ] = {
-  code: 8,
-  callCount: 4,
-  request: {
-    tableName:
-      'projects/{{projectId}}/instances/fake-instance/tables/fake-table',
-    rows: {
-      rowKeys: [],
-      rowRanges: [{}],
+  result: 'error',
+  output: {
+    request: {
+      tableName:
+        'projects/{{projectId}}/instances/fake-instance/tables/fake-table',
+      rows: {
+        rowKeys: [],
+        rowRanges: [{}],
+      },
+      filter: null,
+      rowsLimit: '0',
+      appProfileId: '',
     },
-    filter: null,
-    rowsLimit: '0',
-    appProfileId: '',
+    code: 8,
+    callCount: 4,
   },
 };
 
 exports[
   'Bigtable/ReadRows with a mock server that always sends an error back where the error is retryable should ensure correct behavior with aborted error 1'
 ] = {
-  code: 10,
-  callCount: 4,
-  request: {
-    tableName:
-      'projects/{{projectId}}/instances/fake-instance/tables/fake-table',
-    rows: {
-      rowKeys: [],
-      rowRanges: [{}],
+  result: 'error',
+  output: {
+    request: {
+      tableName:
+        'projects/{{projectId}}/instances/fake-instance/tables/fake-table',
+      rows: {
+        rowKeys: [],
+        rowRanges: [{}],
+      },
+      filter: null,
+      rowsLimit: '0',
+      appProfileId: '',
     },
-    filter: null,
-    rowsLimit: '0',
-    appProfileId: '',
+    code: 10,
+    callCount: 4,
   },
 };
 
 exports[
   'Bigtable/ReadRows with a mock server that always sends an error back where the error is retryable should ensure correct behavior with unavailable error 1'
 ] = {
-  code: 14,
-  callCount: 4,
-  request: {
-    tableName:
-      'projects/{{projectId}}/instances/fake-instance/tables/fake-table',
-    rows: {
-      rowKeys: [],
-      rowRanges: [{}],
+  result: 'error',
+  output: {
+    request: {
+      tableName:
+        'projects/{{projectId}}/instances/fake-instance/tables/fake-table',
+      rows: {
+        rowKeys: [],
+        rowRanges: [{}],
+      },
+      filter: null,
+      rowsLimit: '0',
+      appProfileId: '',
     },
-    filter: null,
-    rowsLimit: '0',
-    appProfileId: '',
+    code: 14,
+    callCount: 4,
+  },
+};
+
+exports[
+  'Bigtable/ReadRows with a mock server that always sends an error back where the error is not retryable should ensure correct behavior with cancelled error 1'
+] = {
+  result: 'data',
+  output: {
+    request: {
+      tableName:
+        'projects/{{projectId}}/instances/fake-instance/tables/fake-table',
+      rows: {
+        rowKeys: [],
+        rowRanges: [{}],
+      },
+      filter: null,
+      rowsLimit: '0',
+      appProfileId: '',
+    },
+    code: 1,
+    callCount: 1,
+  },
+  data: [],
+};
+
+exports[
+  'Bigtable/ReadRows with a mock server that always sends an error back where the error is not retryable should ensure correct behavior with internal error 1'
+] = {
+  result: 'error',
+  output: {
+    request: {
+      tableName:
+        'projects/{{projectId}}/instances/fake-instance/tables/fake-table',
+      rows: {
+        rowKeys: [],
+        rowRanges: [{}],
+      },
+      filter: null,
+      rowsLimit: '0',
+      appProfileId: '',
+    },
+    code: 13,
+    callCount: 1,
+  },
+};
+
+exports[
+  'Bigtable/ReadRows with a mock server that always sends an error back where the error is not retryable should ensure correct behavior with invalid argument error 1'
+] = {
+  result: 'error',
+  output: {
+    request: {
+      tableName:
+        'projects/{{projectId}}/instances/fake-instance/tables/fake-table',
+      rows: {
+        rowKeys: [],
+        rowRanges: [{}],
+      },
+      filter: null,
+      rowsLimit: '0',
+      appProfileId: '',
+    },
+    code: 3,
+    callCount: 1,
   },
 };
