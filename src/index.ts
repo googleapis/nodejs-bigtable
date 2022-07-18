81ea5f7817abc2487fc339de878a15995091650e
danieljbruce <danieljbruce@users.noreply.github.com>
2022-07-18 11:19:37 -0400

    refactor: Pull code to compute location into one function (#1094)
    * snapshot tests for create cluster
    
    * change request to config
    
    * More snapshot tests
    
    * Refactor constants out
    
    * Add generated testcases for partial cluster update
    
    * Add header to constants file
    
    * Remove need to pass location along
    
    * Remove comment
    
    * Use new function to calculate location
    
    * Add space to stay consistent
    
    * Use getClusterBaseConfigWithFullLocation again
    
    * Fix test
    
    * linting fix
    
    * eliminate compile error
    
    * lint fix
    
    * remove the stubs that are not used anymore
    
    * Add an assert statement for location

--- src/index.ts
+++ src/index.ts
@@ -612,15 +612,9 @@ export class Bigtable {
         );
       }
       ClusterUtils.validateClusterMetadata(cluster);
-      const clusterClone = Object.assign({}, cluster);
-      if (clusterClone.location) {
-        clusterClone.location = Cluster.getLocation_(
-          this.projectId,
-          clusterClone.location
-        );
-      }
-      clusters[cluster.id!] = ClusterUtils.getClusterBaseConfig(
-        clusterClone,
+      clusters[cluster.id!] = ClusterUtils.getClusterBaseConfigWithFullLocation(
+        cluster,
+        this.projectId,
         undefined
       );
       Object.assign(clusters[cluster.id!], {
