701badc100853a390494e926b46ecc99446edeee
Daniel Bruce <djbruce@google.com>
2022-09-09 14:58:13 -0400

    Revert "src test and protos copied over"
    This reverts commit 46ce5b8009a001b1197fbf9bbdded805a7ba3def.

--- src/v2/index.ts
+++ src/v2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2022 Google LLC
+// Copyright 2019 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,3 +18,4 @@
 
 export {BigtableInstanceAdminClient} from './bigtable_instance_admin_client';
 export {BigtableTableAdminClient} from './bigtable_table_admin_client';
+export {BigtableClient} from './bigtable_client';
