e982463866cee03f9da5ef64926cc5f5317f8290
Krishanu Dutta Bhuyan <krishanudb@google.com>
2023-05-15 20:13:29 +0530

    fix: Clean up for lint errors

--- src/mutation.ts
+++ src/mutation.ts
@@ -381,15 +381,15 @@ export class Mutation {
    */
   static parseColumnName(columnName: string): ParsedColumn {
     const colonIdx = columnName.indexOf(':');
-      
-    if (colonIdx == -1) {
+
+    if (colonIdx === -1) {
       // columnName does not contain ':'
       return {
         family: columnName,
         qualifier: null,
       };
     }
-      
+ 
     return {
       family: columnName.slice(0, colonIdx),
       qualifier: columnName.slice(colonIdx + 1),
