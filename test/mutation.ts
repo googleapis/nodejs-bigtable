39fdf60c03e9cd94efde3394c5a3a9fd569d85e1
kdb223 <krishanudb@google.com>
2023-05-17 10:42:21 +0530

    revert: "fix: Update test to check null qualifiers instead of undefined"
    This reverts commit 1275dd56cd8aea668703e1f83daecfd14d5199f6.

--- test/mutation.ts
+++ test/mutation.ts
@@ -458,7 +458,7 @@ describe('Bigtable/Mutation', () => {
       const parsed = Mutation.parseColumnName('a');
 
       assert.strictEqual(parsed.family, 'a');
-      assert.strictEqual(parsed.qualifier, null);
+      assert.strictEqual(parsed.qualifier, undefined);
     });
   });
 
