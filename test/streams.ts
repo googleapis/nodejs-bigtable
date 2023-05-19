318aeff453ef17c06a23ca8b0185d22b9f002b34
Alexander Fenster <fenster@google.com>
2023-05-19 02:21:47 +0000

    test: test ReadRows logic with local gRPC server

---
+++ test/streams.ts
@@ -0,0 +1,254 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {before, describe, it} from 'mocha';
+import {Bigtable, Row, Table} from '../src';
+import * as assert from 'assert';
+import {Transform, PassThrough, pipeline} from 'stream';
+
+import {GoogleError} from 'google-gax';
+import {MockServer} from '../src/util/mock-servers/mock-server';
+import {BigtableClientMockService} from '../src/util/mock-servers/service-implementations/bigtable-client-mock-service';
+import {MockService} from '../src/util/mock-servers/mock-service';
+import {debugLog, readRowsImpl} from './utils/readRowsImpl';
+
+describe('Bigtable/Streams', () => {
+  let server: MockServer;
+  let service: MockService;
+  let bigtable: Bigtable;
+  let table: Table;
+
+  before(() => {
+    server = new MockServer(() => {
+      bigtable = new Bigtable({
+        apiEndpoint: `localhost:${server.port}`,
+      });
+      table = bigtable.instance('fake-instance').table('fake-table');
+    });
+    service = new BigtableClientMockService(server);
+  });
+
+  it('should create read stream and read synchronously', done => {
+    const keyFrom = 0;
+    const keyTo = 1000;
+
+    service.setService({
+      ReadRows: readRowsImpl(keyFrom, keyTo),
+    });
+
+    let receivedRowCount = 0;
+    let lastKeyReceived: number | undefined;
+
+    const readStream = table.createReadStream();
+    readStream.on('error', (err: GoogleError) => {
+      done(err);
+    });
+    readStream.on('data', (row: Row) => {
+      ++receivedRowCount;
+      const key = parseInt(row.id);
+      if (lastKeyReceived && key <= lastKeyReceived) {
+        done(new Error('Test error: keys are not in order'));
+      }
+      lastKeyReceived = key;
+      debugLog(`received row key ${key}`);
+    });
+    readStream.on('end', () => {
+      assert.strictEqual(receivedRowCount, keyTo - keyFrom);
+      assert.strictEqual(lastKeyReceived, keyTo - 1);
+      done();
+    });
+  });
+
+  it('should create read stream and read synchronously using Transform stream', done => {
+    const keyFrom = 0;
+    const keyTo = 1000;
+
+    service.setService({
+      ReadRows: readRowsImpl(keyFrom, keyTo),
+    });
+
+    let receivedRowCount = 0;
+    let lastKeyReceived: number | undefined;
+
+    // BigTable stream
+    const readStream = table.createReadStream();
+
+    // Transform stream
+    const transform = new Transform({
+      objectMode: true,
+      transform: (row, _encoding, callback) => {
+        callback(null, row);
+      },
+    });
+
+    // Final stream
+    const passThrough = new PassThrough({
+      objectMode: true,
+    });
+
+    passThrough.on('error', (err: GoogleError) => {
+      done(err);
+    });
+    passThrough.on('data', (row: Row) => {
+      ++receivedRowCount;
+      const key = parseInt(row.id);
+      if (lastKeyReceived && key <= lastKeyReceived) {
+        done(new Error('Test error: keys are not in order'));
+      }
+      lastKeyReceived = key;
+      debugLog(`received row key ${key}`);
+    });
+    passThrough.on('end', () => {
+      assert.strictEqual(receivedRowCount, keyTo - keyFrom);
+      assert.strictEqual(lastKeyReceived, keyTo - 1);
+      done();
+    });
+
+    pipeline(readStream, transform, passThrough, () => {});
+  });
+
+  // TODO(@alexander-fenster): enable after https://github.com/googleapis/nodejs-bigtable/issues/607 is fixed
+  it.skip('should create read stream and read asynchronously using Transform stream', done => {
+    const keyFrom = 0;
+    const keyTo = 1000;
+
+    service.setService({
+      ReadRows: readRowsImpl(keyFrom, keyTo),
+    });
+
+    let receivedRowCount = 0;
+    let lastKeyReceived: number | undefined;
+
+    // BigTable stream
+    const readStream = table.createReadStream();
+
+    // Transform stream
+    const transform = new Transform({
+      objectMode: true,
+      transform: (row, _encoding, callback) => {
+        setTimeout(() => {
+          callback(null, row);
+        }, 0);
+      },
+    });
+
+    // Final stream
+    const passThrough = new PassThrough({
+      objectMode: true,
+    });
+
+    passThrough.on('error', (err: GoogleError) => {
+      done(err);
+    });
+    passThrough.on('data', (row: Row) => {
+      ++receivedRowCount;
+      const key = parseInt(row.id);
+      if (lastKeyReceived && key <= lastKeyReceived) {
+        done(new Error('Test error: keys are not in order'));
+      }
+      lastKeyReceived = key;
+      debugLog(`received row key ${key}`);
+    });
+    passThrough.on('end', () => {
+      assert.strictEqual(receivedRowCount, keyTo - keyFrom);
+      assert.strictEqual(lastKeyReceived, keyTo - 1);
+      done();
+    });
+
+    pipeline(readStream, transform, passThrough, () => {});
+  });
+
+  it('should be able to stop reading from the read stream', done => {
+    const keyFrom = 0;
+    const keyTo = 1000;
+    const stopAfter = 42;
+
+    service.setService({
+      ReadRows: readRowsImpl(keyFrom, keyTo),
+    });
+
+    let receivedRowCount = 0;
+    let lastKeyReceived: number | undefined;
+
+    const readStream = table.createReadStream({
+      // workaround for https://github.com/grpc/grpc-node/issues/2446, remove when fixed
+      gaxOptions: {
+        timeout: 3000,
+      },
+    });
+    readStream.on('error', (err: GoogleError) => {
+      done(err);
+    });
+    readStream.on('data', (row: Row) => {
+      ++receivedRowCount;
+      const key = parseInt(row.id);
+      if (lastKeyReceived && key <= lastKeyReceived) {
+        done(new Error('Test error: keys are not in order'));
+      }
+      lastKeyReceived = key;
+      debugLog(`received row key ${key}`);
+
+      if (receivedRowCount === stopAfter) {
+        debugLog(`requesting to stop after receiving key ${key}`);
+        readStream.end();
+      }
+    });
+    readStream.on('end', () => {
+      assert.strictEqual(receivedRowCount, stopAfter);
+      assert.strictEqual(lastKeyReceived, stopAfter - 1);
+      done();
+    });
+  });
+
+  // TODO(@alexander-fenster): enable after the resumption logic is fixed.
+  // Currently, lastRowKey is updated in _transform (chunktransfomer.ts:155)
+  // https://github.com/googleapis/nodejs-bigtable/blob/436e77807e87e13f80ac2bc2c43813b09090000f/src/chunktransformer.ts#L155
+  // before the record is committed, which makes it omit this record when
+  // the call is resumed. I believe lastRowKey should only be set in commit().
+  it.skip('should silently resume after server or network error', done => {
+    const keyFrom = 0;
+    const keyTo = 1000;
+    const errorAfterChunkNo = 423;
+
+    service.setService({
+      ReadRows: readRowsImpl(keyFrom, keyTo, errorAfterChunkNo),
+    });
+
+    let receivedRowCount = 0;
+    let lastKeyReceived: number | undefined;
+
+    const readStream = table.createReadStream();
+    readStream.on('error', (err: GoogleError) => {
+      done(err);
+    });
+    readStream.on('data', (row: Row) => {
+      ++receivedRowCount;
+      const key = parseInt(row.id);
+      if (lastKeyReceived && key <= lastKeyReceived) {
+        done(new Error('Test error: keys are not in order'));
+      }
+      lastKeyReceived = key;
+      debugLog(`received row key ${key}`);
+    });
+    readStream.on('end', () => {
+      assert.strictEqual(receivedRowCount, keyTo - keyFrom);
+      assert.strictEqual(lastKeyReceived, keyTo - 1);
+      done();
+    });
+  });
+
+  after(async () => {
+    server.shutdown(() => {});
+  });
+});
---
+++ test/utils/readRowsImpl.ts
@@ -0,0 +1,263 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {ServerWritableStream} from '@grpc/grpc-js';
+import {protos} from '../../src';
+import {GoogleError, Status} from 'google-gax';
+
+const valueSize = 1024 * 1024;
+const chunkSize = 1023 * 1024 - 1; // make it uneven
+const chunksPerResponse = 10;
+
+const debug = process.env.BIGTABLE_TEST_DEBUG === 'true';
+
+export function debugLog(text: string) {
+  if (debug) {
+    console.log(text);
+  }
+}
+
+function generateChunks(
+  keyFrom: number,
+  keyTo: number,
+  stream: ServerWritableStream<
+    protos.google.bigtable.v2.IReadRowsRequest,
+    protos.google.bigtable.v2.IReadRowsResponse
+  >
+) {
+  debugLog(`generating chunks from ${keyFrom} to ${keyTo}`);
+
+  const chunks: protos.google.bigtable.v2.ReadRowsResponse.ICellChunk[] = [];
+  for (let key = keyFrom; key < keyTo; ++key) {
+    const binaryKey = Buffer.from(key.toString().padStart(8, '0'));
+
+    // primitive support for row ranges
+    if (stream.request.rows?.rowRanges || stream.request.rows?.rowKeys) {
+      let take = false;
+      const stringKey = binaryKey.toString();
+      for (const requestKey of stream.request.rows?.rowKeys ?? []) {
+        if (stringKey === requestKey.toString()) {
+          take;
+          break;
+        }
+      }
+      for (const range of stream.request.rows?.rowRanges ?? []) {
+        let startOk = true;
+        let endOk = true;
+        if (range.startKeyOpen && range.startKeyOpen.toString() >= stringKey) {
+          startOk = false;
+        }
+        if (
+          range.startKeyClosed &&
+          range.startKeyClosed.toString() > stringKey
+        ) {
+          startOk = false;
+        }
+        if (range.endKeyOpen && range.endKeyOpen.toString() <= stringKey) {
+          endOk = false;
+        }
+        if (range.endKeyClosed && range.endKeyClosed.toString() < stringKey) {
+          endOk = false;
+        }
+        if (startOk && endOk) {
+          take = true;
+        }
+      }
+      if (!take) {
+        debugLog(
+          `skipping key ${key} because it's out of requested range or keys`
+        );
+        continue;
+      }
+    }
+
+    debugLog(`generating chunks for ${key}`);
+    const rowKey = binaryKey.toString('base64');
+    let remainingBytes = valueSize;
+    let chunkCounter = 0;
+    while (remainingBytes > 0) {
+      debugLog(`  remaining bytes: ${remainingBytes}`);
+      const chunk: protos.google.bigtable.v2.ReadRowsResponse.ICellChunk = {};
+      if (chunkCounter === 0) {
+        chunk.rowKey = rowKey;
+        chunk.familyName = {
+          value: 'family',
+        };
+        chunk.qualifier = {
+          value: Buffer.from('qualifier').toString('base64'),
+        };
+      }
+      const thisChunkSize =
+        remainingBytes >= chunkSize ? chunkSize : remainingBytes;
+      remainingBytes -= thisChunkSize;
+      const value = Buffer.from('a'.repeat(remainingBytes)).toString('base64');
+      chunk.value = value;
+      if (remainingBytes === 0) {
+        debugLog(`  setting commit flag for rowKey ${key}`);
+        chunk.commitRow = true;
+      }
+      chunks.push(chunk);
+      ++chunkCounter;
+    }
+  }
+  debugLog(`generated ${chunks.length} chunks between ${keyFrom} and ${keyTo}`);
+  return chunks;
+}
+
+export function readRowsImpl(
+  keyFrom: number,
+  keyTo: number,
+  errorAfterChunkNo?: number
+) {
+  return async (
+    stream: ServerWritableStream<
+      protos.google.bigtable.v2.IReadRowsRequest,
+      protos.google.bigtable.v2.IReadRowsResponse
+    >
+  ) => {
+    // pretty-printing important parts of the request.
+    // doing it field by field because we want to apply .toString() to all key fields
+    debugLog('received request: {');
+    debugLog(`  tableName: "${stream.request.tableName}",`);
+    if (stream.request.rows) {
+      debugLog('  rows: {');
+      if (stream.request.rows.rowKeys) {
+        debugLog('    rowKeys: [');
+        for (const key of stream.request.rows.rowKeys) {
+          debugLog(`      "${key.toString()}",`);
+        }
+        debugLog('    ],');
+      }
+      if (stream.request.rows.rowRanges) {
+        debugLog('    rowRanges: [');
+        for (const range of stream.request.rows.rowRanges) {
+          debugLog('      {');
+          if (range.startKeyOpen) {
+            debugLog(
+              `        startKeyOpen: "${range.startKeyOpen.toString()}",`
+            );
+          }
+          if (range.startKeyClosed) {
+            debugLog(
+              `        startKeyClosed: "${range.startKeyClosed.toString()}",`
+            );
+          }
+          if (range.endKeyOpen) {
+            debugLog(`        endKeyOpen: "${range.endKeyOpen.toString()}",`);
+          }
+          if (range.endKeyClosed) {
+            debugLog(
+              `        endKeyClosed: "${range.endKeyClosed.toString()}",`
+            );
+          }
+          debugLog('      },');
+        }
+        debugLog('    ],');
+      }
+      debugLog('  },');
+    }
+    debugLog('}');
+
+    let stopWaiting: () => void = () => {};
+    let cancelled = false;
+
+    stream.on('cancelled', () => {
+      debugLog('gRPC server received cancel()');
+      cancelled = true;
+      stopWaiting();
+      stream.emit('error', new Error('Cancelled'));
+    });
+
+    let chunksSent = 0;
+    const chunks = generateChunks(keyFrom, keyTo, stream);
+    let lastScannedRowKey: string | undefined;
+    let firstN: protos.google.bigtable.v2.ReadRowsResponse.ICellChunk[] = [];
+    let chunkIdx = 0;
+    for (const chunk of chunks) {
+      if (cancelled) {
+        break;
+      }
+      if (chunk.rowKey) {
+        debugLog(
+          `starting row with key ${Buffer.from(
+            chunk.rowKey as string,
+            'base64'
+          ).toString()}`
+        );
+        lastScannedRowKey = chunk.rowKey as string;
+      }
+      if (chunk.commitRow) {
+        debugLog('commit row');
+      }
+      firstN.push(chunk);
+      ++chunkIdx;
+      if (
+        firstN.length === chunksPerResponse ||
+        chunkIdx === errorAfterChunkNo
+      ) {
+        const response: protos.google.bigtable.v2.IReadRowsResponse = {
+          chunks: firstN,
+          lastScannedRowKey,
+        };
+        chunksSent += firstN.length;
+        await new Promise<void>(resolve => {
+          setTimeout(async () => {
+            if (cancelled) {
+              resolve();
+              return;
+            }
+            const canSendMore = stream.write(response);
+            debugLog(`sent ${firstN.length} chunks`);
+            firstN = [];
+            if (!canSendMore) {
+              debugLog(
+                `awaiting for back pressure after sending ${chunksSent} chunks`
+              );
+              await new Promise<void>(resolve => {
+                stopWaiting = resolve;
+                stream.once('drain', resolve);
+              });
+            }
+            resolve();
+          }, 0);
+        });
+        if (chunkIdx === errorAfterChunkNo) {
+          debugLog(`sending error after chunk #${chunkIdx}`);
+          errorAfterChunkNo = undefined; // do not send error for the second time
+          const error = new GoogleError('Uh oh');
+          error.code = Status.ABORTED;
+          stream.emit('error', error);
+          cancelled = true;
+          break;
+        }
+      }
+    }
+    if (!cancelled && firstN.length > 0) {
+      const response: protos.google.bigtable.v2.IReadRowsResponse = {
+        chunks: firstN,
+        lastScannedRowKey,
+      };
+      chunksSent += firstN.length;
+      await new Promise<void>(resolve => {
+        setTimeout(() => {
+          stream.write(response);
+          resolve();
+        }, 0);
+      });
+      debugLog(`sent ${firstN.length} remaining chunks`);
+    }
+    debugLog(`in total, sent ${chunksSent} chunks`);
+    stream.end();
+  };
+}
