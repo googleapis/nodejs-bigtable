11cd798016c009adfa24d2b464aa3f1efd0827a9
Daniel Bruce <djbruce@google.com>
2023-10-03 17:29:20 -0400

    Add echo to test

--- .kokoro/conformance.sh
+++ .kokoro/conformance.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+echo "test1"
+
 set -eo pipefail
 
 export NPM_CONFIG_PREFIX=${HOME}/.npm-global
