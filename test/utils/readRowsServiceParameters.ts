2ba1caf9a3e1ac1287d850a8e3de031a56e52a46
Daniel Bruce <djbruce@google.com>
2024-07-17 14:45:08 -0400

    Add some comments for documentation

--- test/utils/readRowsServiceParameters.ts
+++ test/utils/readRowsServiceParameters.ts
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/**
+ * This file contains the parameters for the readRowsService.
+ */
+
 export interface ReadRowsServiceParameters {
   keyFrom: number;
   keyTo: number;
