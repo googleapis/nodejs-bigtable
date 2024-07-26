11b2b60ae8aaca218902473345a330aa29b79ea5
Daniel Bruce <djbruce@google.com>
2024-07-26 17:05:08 -0400

    Create a user stream class
    This allows us to override the write method

--- src/table.ts
+++ src/table.ts
@@ -754,6 +754,62 @@ Please use the format 'prezzy' or '${instance.name}/tables/prezzy'.`);
     // discarded in the per attempt subpipeline (rowStream)
     let lastRowKey = '';
     let rowsRead = 0;
+
+    class UserStream extends Transform {
+      constructor() {
+        super({
+          objectMode: true,
+          readableHighWaterMark: 0, // We need to disable readside buffering to allow for acceptable behavior when the end user cancels the stream early.
+          writableHighWaterMark: 0, // We need to disable writeside buffering because in nodejs 14 the call to _transform happens after write buffering. This creates problems for tracking the last seen row key.
+          transform(row, _encoding, callback) {
+            if (userCanceled) {
+              callback();
+              return;
+            }
+            if (TableUtils.lessThanOrEqualTo(row.id, lastRowKey)) {
+              /*
+              Sometimes duplicate rows reach this point. To avoid delivering
+              duplicate rows to the user, rows are thrown away if they don't exceed
+              the last row key. We can expect each row to reach this point and rows
+              are delivered in order so if the last row key equals or exceeds the
+              row id then we know data for this row has already reached this point
+              and been delivered to the user. In this case we want to throw the row
+              away and we do not want to deliver this row to the user again.
+               */
+              callback();
+              return;
+            }
+            lastRowKey = row.id;
+            rowsRead++;
+            callback(null, row);
+          },
+        });
+      }
+      write(
+        chunk: any,
+        encoding?: BufferEncoding,
+        cb?: (error: Error | null | undefined) => void
+      ): boolean;
+      write(
+        chunk: any,
+        cb?: (error: Error | null | undefined) => void
+      ): boolean;
+      write(
+        chunk: any,
+        encodingOrCb?: BufferEncoding | ((error?: Error | null) => void),
+        callback?: (error?: Error | null) => void
+      ) {
+        if (callback) {
+          return super.write(chunk, encodingOrCb as BufferEncoding, callback);
+        }
+        return super.write(
+          chunk,
+          encodingOrCb as (error?: Error | null) => void
+        );
+      }
+    }
+    const userStream = new UserStream() as Transform;
+    /*
     const userStream = new PassThrough({
       objectMode: true,
       readableHighWaterMark: 0, // We need to disable readside buffering to allow for acceptable behavior when the end user cancels the stream early.
@@ -764,15 +820,6 @@ Please use the format 'prezzy' or '${instance.name}/tables/prezzy'.`);
           return;
         }
         if (TableUtils.lessThanOrEqualTo(row.id, lastRowKey)) {
-          /*
-          Sometimes duplicate rows reach this point. To avoid delivering
-          duplicate rows to the user, rows are thrown away if they don't exceed
-          the last row key. We can expect each row to reach this point and rows
-          are delivered in order so if the last row key equals or exceeds the
-          row id then we know data for this row has already reached this point
-          and been delivered to the user. In this case we want to throw the row
-          away and we do not want to deliver this row to the user again.
-           */
           callback();
           return;
         }
@@ -781,6 +828,7 @@ Please use the format 'prezzy' or '${instance.name}/tables/prezzy'.`);
         callback(null, row);
       },
     });
+    */
 
     // The caller should be able to call userStream.end() to stop receiving
     // more rows and cancel the stream prematurely. But also, the 'end' event
