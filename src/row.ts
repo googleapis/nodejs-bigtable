932ab9e4af255e3346eb1700dade918bc3151439
danieljbruce <danieljbruce@users.noreply.github.com>
2024-08-28 13:43:08 -0400

    feat: Bigtable authorized views - move the code over to the TabularApiSurface class (#1463)
    * Move the constructor over to TabularApiService
    
    * Move sampleRowKeys over
    
    * Move sampleRowKeys functions over and use promisify
    
    * Adjust the proxyquire to work with TabularAPIserv
    
    * Move all the ReadRows functionality over
    
    * Solve the issue with the is dependency
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * Add header for new class
    
    * Only include Table mocks that are necessary
    
    * Remove TODO
    
    * Rename TabularApiService to TabularApiSurface
    
    * Change all imports to tabular-api-surface
    
    * surface. not service
    
    ---------
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

--- src/row.ts
+++ src/row.ts
@@ -31,6 +31,7 @@ import {Chunk} from './chunktransformer';
 import {CallOptions} from 'google-gax';
 import {ServiceError} from 'google-gax';
 import {google} from '../protos/protos';
+import {TabularApiSurface} from './tabular-api-surface';
 
 export interface Rule {
   column: string;
@@ -156,13 +157,13 @@ export class RowError extends Error {
  */
 export class Row {
   bigtable: Bigtable;
-  table: Table;
+  table: TabularApiSurface;
   id: string;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   data: any;
   key?: string;
   metadata?: {};
-  constructor(table: Table, key: string) {
+  constructor(table: TabularApiSurface, key: string) {
     this.bigtable = table.bigtable;
     this.table = table;
     this.id = key;
