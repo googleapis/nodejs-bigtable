bbe31994d7dfa35f6eadc0d6f2dd1e783fb7db6a
Daniel Bruce <djbruce@google.com>
2024-09-30 15:54:14 -0400

    Set the ReadRows options

--- test/authorized-views.ts
+++ test/authorized-views.ts
@@ -1,6 +1,7 @@
 import {describe} from 'mocha';
-import assert from 'assert';
-import {AbortableDuplex, Bigtable} from '../src';
+import {AbortableDuplex, Bigtable, PrefixRange, RawFilter} from '../src';
+import {PassThrough} from 'stream';
+import {CallOptions} from 'google-gax';
 
 describe('Bigtable/AuthorizedViews', () => {
   describe('Authorized View methods should have requests that match Table and Row requests', () => {
@@ -14,9 +15,34 @@ describe('Bigtable/AuthorizedViews', () => {
         const table = instance.table(fakeTableName);
         const view = instance.view(fakeTableName, fakeViewName);
         table.bigtable.request = (config: any) => {
-          assert.strictEqual(config.reqOpts.rowsLimit, options.limit);
-          return (stream as {} as AbortableDuplex).abort = () => {};;
+          const stream = new PassThrough({
+            objectMode: true,
+          });
+          return stream as {} as AbortableDuplex;
         };
+        table.createReadStream({
+          decode: true,
+
+          /**
+           * The encoding to use when converting Buffer values to a string.
+           */
+          // encoding?: string;
+
+          /**
+           * End value for key range.
+           */
+          // end?: string;
+
+          /**
+           * Row filters allow you to both make advanced queries and format how the data is returned.
+           */
+          // filter: RawFilter;
+          gaxOptions: {
+            maxRetries: 4,
+          },
+          limit: 5,
+          start: '7',
+        });
       });
     });
   });
