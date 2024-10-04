d9fddafbd14dc77891ae23cdc328efd9e3cb75d7
Daniel Bruce <djbruce@google.com>
2024-10-04 14:43:10 -0400

    Fix the readrows test to end the stream
    Also add a test for insert and make the test easier to debug.

--- test/authorized-views.ts
+++ test/authorized-views.ts
@@ -5,7 +5,7 @@ import * as assert from 'assert';
 import {Mutation} from '../src/mutation';
 import * as mocha from 'mocha';
 
-describe('Bigtable/AuthorizedViews', () => {
+describe.only('Bigtable/AuthorizedViews', () => {
   describe('Authorized View methods should have requests that match Table and Row requests', () => {
     describe('Table', () => {
       const bigtable = new Bigtable({});
@@ -16,88 +16,101 @@ describe('Bigtable/AuthorizedViews', () => {
       const table = instance.table(fakeTableName);
       const view = instance.view(fakeTableName, fakeViewName);
       describe('should make ReadRows grpc requests', () => {
-        function setupReadRows() {
+        function setupReadRows(done: mocha.Done) {
           let requestCount = 0;
           table.bigtable.request = (config: any) => {
-            requestCount++;
-            assert.strictEqual(config.client, 'BigtableClient');
-            assert.strictEqual(config.method, 'readRows');
-            const requestForTable = {
-              tableName: `projects/{{projectId}}/instances/${fakeInstanceName}/tables/${fakeTableName}`,
-            };
-            const requestForAuthorizedView = {
-              authorizedViewName: `projects/{{projectId}}/instances/${fakeInstanceName}/tables/${fakeTableName}/authorizedViews/${fakeViewName}`,
-            };
-            const expectedPartialReqOpts =
-              requestCount === 1 ? requestForTable : requestForAuthorizedView;
-            const expectedReqOpts = Object.assign(
-              {
-                appProfileId: undefined,
-                rows: {
-                  rowKeys: [],
-                  rowRanges: [
-                    {
-                      startKeyClosed: Buffer.from('7'),
-                      endKeyClosed: Buffer.from('9'),
-                    },
-                  ],
+            try {
+              requestCount++;
+              assert.strictEqual(config.client, 'BigtableClient');
+              assert.strictEqual(config.method, 'readRows');
+              const requestForTable = {
+                tableName: `projects/{{projectId}}/instances/${fakeInstanceName}/tables/${fakeTableName}`,
+              };
+              const requestForAuthorizedView = {
+                authorizedViewName: `projects/{{projectId}}/instances/${fakeInstanceName}/tables/${fakeTableName}/authorizedViews/${fakeViewName}`,
+              };
+              const expectedPartialReqOpts =
+                requestCount === 1 ? requestForTable : requestForAuthorizedView;
+              const expectedReqOpts = Object.assign(
+                {
+                  appProfileId: undefined,
+                  rows: {
+                    rowKeys: [],
+                    rowRanges: [
+                      {
+                        startKeyClosed: Buffer.from('7'),
+                        endKeyClosed: Buffer.from('9'),
+                      },
+                    ],
+                  },
+                  filter: {
+                    columnQualifierRegexFilter: Buffer.from('abc'),
+                  },
+                  rowsLimit: 5,
                 },
-                filter: {
-                  columnQualifierRegexFilter: Buffer.from('abc'),
+                expectedPartialReqOpts
+              );
+              assert.deepStrictEqual(config.reqOpts, expectedReqOpts);
+              const expectedGaxOpts = {
+                maxRetries: 4,
+                otherArgs: {
+                  headers: {
+                    'bigtable-attempt': 0,
+                  },
                 },
-                rowsLimit: 5,
-              },
-              expectedPartialReqOpts
-            );
-            assert.deepStrictEqual(config.reqOpts, expectedReqOpts);
-            const expectedGaxOpts = {
-              maxRetries: 4,
-              otherArgs: {
-                headers: {
-                  'bigtable-attempt': 0,
-                },
-              },
-            };
-            assert.deepStrictEqual(config.gaxOpts, expectedGaxOpts);
+              };
+              assert.deepStrictEqual(config.gaxOpts, expectedGaxOpts);
+            } catch (err: unknown) {
+              done(err);
+            }
             const stream = new PassThrough({
               objectMode: true,
+            });
+            setImmediate(() => {
+              stream.end();
             });
             return stream as {} as AbortableDuplex;
           };
         }
 
-        it('requests for createReadStream should match', async () => {
-          setupReadRows();
-          const opts = {
-            decode: true,
-            end: '9',
-            filter: [{column: 'abc'}],
-            gaxOptions: {
-              maxRetries: 4,
-            },
-            limit: 5,
-            start: '7',
-          };
-          await table.createReadStream(opts);
-          await view.createReadStream(opts);
+        it('requests for createReadStream should match', done => {
+          setupReadRows(done);
+          (async () => {
+            const opts = {
+              decode: true,
+              end: '9',
+              filter: [{column: 'abc'}],
+              gaxOptions: {
+                maxRetries: 4,
+              },
+              limit: 5,
+              start: '7',
+            };
+            await table.createReadStream(opts);
+            await view.createReadStream(opts);
+            done();
+          })();
         });
-        it('requests for getRows should match', async () => {
-          setupReadRows();
-          const opts = {
-            decode: true,
-            end: '9',
-            filter: [{column: 'abc'}],
-            gaxOptions: {
-              maxRetries: 4,
-            },
-            limit: 5,
-            start: '7',
-          };
-          await table.getRows(opts);
-          await view.getRows(opts);
+        it('requests for getRows should match', done => {
+          setupReadRows(done);
+          (async () => {
+            const opts = {
+              decode: true,
+              end: '9',
+              filter: [{column: 'abc'}],
+              gaxOptions: {
+                maxRetries: 4,
+              },
+              limit: 5,
+              start: '7',
+            };
+            await table.getRows(opts);
+            await view.getRows(opts);
+            done();
+          })();
         });
       });
-      describe.only('should make MutateRows grpc requests', () => {
+      describe('should make MutateRows grpc requests', () => {
         function setupMutateRows(done: mocha.Done) {
           let requestCount = 0;
           table.bigtable.request = (config: any) => {
@@ -178,6 +191,30 @@ describe('Bigtable/AuthorizedViews', () => {
             await table.mutate(mutation, {gaxOptions});
             view.maxRetries = 0;
             await view.mutate(mutation, {gaxOptions});
+            done();
+          })();
+        });
+        it('requests for insert should match', done => {
+          (async () => {
+            setupMutateRows(done);
+            const mutation = {
+              key: 'some-id',
+              data: {
+                follows: {
+                  tjefferson: {
+                    value: 1,
+                    timestamp: 2,
+                  },
+                },
+              },
+            };
+            // Currently the client retries on an end event if all the data
+            // hasn't been sent back so maxRetries needs to be 0.
+            const gaxOptions = {maxRetries: 0};
+            table.maxRetries = 0;
+            await table.insert(mutation, gaxOptions);
+            view.maxRetries = 0;
+            await view.insert(mutation, gaxOptions);
             done();
           })();
         });
