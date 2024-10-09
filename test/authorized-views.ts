39d0bb6194858e7dbcebfd2b3ddfd9da8f4b2f63
Daniel Bruce <djbruce@google.com>
2024-10-09 11:19:15 -0400

    Add header to the test

--- test/authorized-views.ts
+++ test/authorized-views.ts
@@ -1,3 +1,17 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import {beforeEach, describe} from 'mocha';
 import {AbortableDuplex, Bigtable, RawFilter, RequestCallback} from '../src';
 import {PassThrough} from 'stream';
@@ -5,7 +19,6 @@ import * as assert from 'assert';
 import {Mutation} from '../src/mutation';
 import * as mocha from 'mocha';
 import {Row} from '../src';
-import {ServiceError} from 'google-gax';
 
 describe('Bigtable/AuthorizedViews', () => {
   describe('Authorized View methods should have requests that match Table and Row requests', () => {
