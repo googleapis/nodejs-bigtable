dd98c4e98b7161feec5b6842fd702374feccd6d8
Daniel Bruce <djbruce@google.com>
2024-11-29 14:52:45 -0500

    Add the inverse function

--- src/utils/readModifyWriteRow/getRMWRRequest.ts
+++ src/utils/readModifyWriteRow/getRMWRRequest.ts
@@ -1,6 +1,9 @@
 import {Rule} from '../../row';
 import {Mutation} from '../../mutation';
 import arrify = require('arrify');
+import * as protos from '../../../protos/protos';
+
+type RMRWRequest = protos.google.bigtable.v2.IReadModifyWriteRowRequest;
 
 interface RMWRRequestData {
   reqOpts: {
@@ -12,7 +15,17 @@ interface RMWRRequestData {
   appProfileId?: string;
 }
 
-export function getRMWRRequest(request: RMWRRequestData){
+/**
+ * This function will create a request that can be passed into the
+ * readModifyWriteRequest method in the Gapic layer.
+ *
+ * @param {RMWRRequestData} request The readModifyWrite request information
+ * that will be used to build the readModifyWrite request that will be sent to
+ * the Gapic layer.
+ * @returns {RMRWRequest} A request that can be passed into the
+ * readModifyWriteRow method in the Gapic layer.
+ */
+export function getRMWRRequest(request: RMWRRequestData): RMRWRequest {
   const {reqOpts, id, rules, appProfileId} = request;
   if (!rules || (rules as Rule[]).length === 0) {
     throw new Error('At least one rule must be provided.');
@@ -45,4 +58,35 @@ export function getRMWRRequest(request: RMWRRequestData){
     },
     reqOpts
   );
+}
+
+/**
+ * This function will translate a request that is appropriate for the
+ * readModifyWriteRow method in the Gapic layer into a request for the
+ * handwritten layer that calls the readModifyWriteRow method under the hood.
+ *
+ * @param {RMWRRequest} request The request that is in a format that can be
+ * passed into the readModifyWriteRow Gapic layer method
+ * @return {RMWRRequestData} A readModifyWriteRow request that can be passed
+ * into getRMWRRequest and return the original `request` parameter.
+ */
+export function getRMWRRequestInverse(request: RMRWRequest): RMWRRequestData {
+  return {
+    reqOpts: {},
+    id: request.rowKey as string,
+    rules: [],
+  }; // TODO: Implement this function.
+  /**
+   * This function needs to be written so that for any `request` value we have
+   * request === getRMWRRequestInverse(getRMWRRequest(request)) and
+   * request === getRMWRRequest(getRMWRRequestInverse(request)). It will be used
+   * by the readModifyWriteRow test proxy service to transform a Gapic request
+   * into a request for the handwritten layer. getRMWRRequest and
+   * getRMWRRequestInverse are required to be inverses so that when
+   * getRMWRRequestInverse is applied in the test proxy service and then
+   * getRMWRRequest is applied by the handwritten layer that the Gapic layer
+   * receives the request that was originally passed into the test proxy
+   * service. This will ensure the test proxy is actually testing the request
+   * it is supposed to be testing.
+   */
 }
