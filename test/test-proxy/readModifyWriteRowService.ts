d5cc2db342da8cbb7dc04a81264476dbec67af39
Daniel Bruce <djbruce@google.com>
2024-12-03 16:04:47 -0500

    Add header. remove only.

--- test/test-proxy/readModifyWriteRowService.ts
+++ test/test-proxy/readModifyWriteRowService.ts
@@ -1,3 +1,17 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import * as assert from 'assert';
 
 import {describe} from 'mocha';
@@ -6,7 +20,7 @@ import {BigtableClient} from '../../src/v2';
 const readModifyWriteRowService = require('../../../testproxy/services/read-modify-write-row.js');
 const createClient = require('../../../testproxy/services/create-client.js');
 
-describe.only('TestProxy/ReadModifyWriteRow', () => {
+describe('TestProxy/ReadModifyWriteRow', () => {
   const testCases: protos.google.bigtable.v2.IReadModifyWriteRowRequest[] = [
     {
       tableName: 'projects/projectId/instances/instance/tables/test-table',
