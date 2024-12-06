7d2027f35ab4bf9913388bac077c7f2975c8c751
Daniel Bruce <djbruce@google.com>
2024-12-06 13:11:36 -0500

    Use the timeout in GetRowOptions

--- src/tabular-api-surface.ts
+++ src/tabular-api-surface.ts
@@ -231,10 +231,11 @@ Please use the format 'prezzy' or '${instance.name}/tables/prezzy'.`);
     downstream in google-gax.
      */
     const timeout =
-      this?.bigtable?.options?.BigtableClient?.clientConfig?.interfaces &&
-      this?.bigtable?.options?.BigtableClient?.clientConfig?.interfaces[
-        'google.bigtable.v2.Bigtable'
-      ]?.methods['ReadRows']?.timeout_millis;
+      opts?.gaxOptions?.timeout ||
+      (this?.bigtable?.options?.BigtableClient?.clientConfig?.interfaces &&
+        this?.bigtable?.options?.BigtableClient?.clientConfig?.interfaces[
+          'google.bigtable.v2.Bigtable'
+        ]?.methods['ReadRows']?.timeout_millis);
     const callTimeMillis = new Date().getTime();
 
     const ranges = TableUtils.getRanges(options);
