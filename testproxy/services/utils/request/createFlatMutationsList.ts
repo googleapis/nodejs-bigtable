bb9a2661521b5c80ec889fae623decdf0017329a
Daniel Bruce <djbruce@google.com>
2024-12-10 10:34:24 -0500

    Rename mutations to entries

--- testproxy/services/utils/request/createFlatMutationsList.ts
+++ testproxy/services/utils/request/createFlatMutationsList.ts
@@ -30,7 +30,7 @@ export function createFlatMutationsListWithFn<
  * FilterConfigOption[] by assuming 'f' converts Mutations to their protobuf
  * representation.  Note: This does *not* invert the transformation performed by 'f' itself.
  *
- * @param {T[]} mutations The flattened mutations list.
+ * @param {T[]} entries The flattened mutations list.
  * @param {Function} fInverse The inverse of the function 'f' used in createFlatMutationsListWithFn.  MUST BE PROVIDED and MUST correctly invert 'f'.
  * @param {number} numEntries The original number of entries.  This is REQUIRED as the flattening operation loses this information.
  * @returns {FilterConfigOption[]} The reconstructed FilterConfigOption array.
@@ -38,17 +38,17 @@ export function createFlatMutationsListWithFn<
 export function createFlatMutationsListWithFnInverse<
   T extends google.bigtable.v2.IMutation,
 >(
-  mutations: T[],
+  entries: T[],
   fInverse: (entry: T) => Mutation, //  Type changed to reflect the inverse function
   numEntries: number
 ): FilterConfigOption[] {
   const invertedEntries: FilterConfigOption[] = [];
-  const mutationsPerEntry = mutations.length / numEntries;
+  const mutationsPerEntry = entries.length / numEntries;
 
   for (let i = 0; i < numEntries; i++) {
     const start = i * mutationsPerEntry;
     const end = start + mutationsPerEntry;
-    const entryMutations = mutations.slice(start, end) as unknown as T[]; // Type cast to align with fInverse input
+    const entryMutations = entries.slice(start, end) as unknown as T[]; // Type cast to align with fInverse input
 
     const invertedEntry: FilterConfigOption = {};
     for (const mutation of entryMutations) {
