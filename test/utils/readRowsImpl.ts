e8adb19890592127f2e298d85f50859c57a11861
Kevin Kim <kevkim@google.com>
2025-01-14 14:22:49 -0500

    fix prettier

--- test/utils/readRowsImpl.ts
+++ test/utils/readRowsImpl.ts
@@ -433,7 +433,6 @@ export class ReadRowsImpl {
         chunksSent += currentResponseChunks.length;
         await readRowsRequestHandler.sendResponse(response);
         currentResponseChunks = [];
-        console.log(currentResponseChunks)
 
         if (chunkIdx === this.errorAfterChunkNo) {
           debugLog(`sending error after chunk #${chunkIdx}`);
