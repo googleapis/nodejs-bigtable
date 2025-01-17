0ac6d15aa8436027e042f2e6e5f1594cf6636e3f
Daniel Bruce <djbruce@google.com>
2025-01-17 13:26:09 -0500

    Generate documentation for the date provider

--- common/test-date-provider.ts
+++ common/test-date-provider.ts
@@ -1,18 +1,38 @@
 import {WithLogger} from './logger';
 
+/**
+ * A test implementation of a Date-like object.  Used for testing purposes.  It provides a
+ * getTime method that returns a pre-determined fake date value, allowing for
+ * deterministic testing of time-dependent functionality.
+ */
 class TestDateLike {
   private fakeDate;
+  /**
+   * @param {number} fakeDate The fake date value to be returned by getTime(), in milliseconds.
+   */
   constructor(fakeDate: number) {
     this.fakeDate = fakeDate;
   }
+  /**
+   * Returns the fake date value that this object was created with.
+   * @returns {number} The fake date, in milliseconds.
+   */
   getTime() {
     return this.fakeDate;
   }
 }
 
-// TODO: ILogger in separate file
+/**
+ * A test implementation of a DateProvider. Used for testing purposes. Provides
+ * a deterministic series of fake dates, with each call to getDate() returning a date 1000ms later than the last.
+ * Logs each date value returned for verification purposes.
+ */
 export class TestDateProvider extends WithLogger {
   private dateCounter = 0;
+  /**
+   * Returns a new fake date 1000ms later than the last. Logs the date for test verification.
+   * @returns {TestDateLike} A fake date object.
+   */
   getDate() {
     // The test assumes exactly 1s passes between each getDate call.
     this.dateCounter = this.dateCounter + 1000;
