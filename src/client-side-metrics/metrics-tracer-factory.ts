d5a0368497c61e8a56422f5ee6b7d94e3f1a6889
Daniel Bruce <djbruce@google.com>
2025-01-20 14:57:01 -0500

    Require a call to fetch the project when using MT

--- src/client-side-metrics/metrics-tracer-factory.ts
+++ src/client-side-metrics/metrics-tracer-factory.ts
@@ -478,90 +478,103 @@ class MetricsTracer {
  * OpenTelemetry metrics instruments.
  */
 export class MetricsTracerFactory {
-  private metrics: Metrics;
+  private metrics?: Metrics;
+  private observabilityOptions?: ObservabilityOptions;
 
   /**
    * @param observabilityOptions Options for configuring client-side metrics observability.
    */
   constructor(observabilityOptions?: ObservabilityOptions) {
-    // Use MeterProvider provided by user
-    // If MeterProvider was not provided then use the default meter provider.
-    const meterProvider =
-      observabilityOptions && observabilityOptions.meterProvider
-        ? observabilityOptions.meterProvider
-        : new MeterProvider({
-            // This is the default meter provider
-            // Create a resource. Fill the `service.*` attributes in with real values for your service.
-            // GcpDetectorSync will add in resource information about the current environment if you are
-            // running on GCP. These resource attributes will be translated to a specific GCP monitored
-            // resource if running on GCP. Otherwise, metrics will be sent with monitored resource
-            // `generic_task`.
-            resource: new Resources.Resource({
-              'service.name': 'bigtable-metrics',
-            }).merge(new ResourceUtil.GcpDetectorSync().detect()),
-            readers: [
-              // Register the exporter
-              new PeriodicExportingMetricReader({
-                // Export metrics every 10 seconds. 5 seconds is the smallest sample period allowed by
-                // Cloud Monitoring.
-                exportIntervalMillis: 10_000,
-                exporter: new MetricExporter({
-                  projectId: 'cloud-native-db-dpes-shared', // TODO: Replace later
+    this.observabilityOptions = observabilityOptions;
+  }
+
+  private initialize(
+    projectId?: string,
+    observabilityOptions?: ObservabilityOptions
+  ) {
+    if (this.metrics) {
+      return this.metrics;
+    } else {
+      // Use MeterProvider provided by user
+      // If MeterProvider was not provided then use the default meter provider.
+      const meterProvider =
+        observabilityOptions && observabilityOptions.meterProvider
+          ? observabilityOptions.meterProvider
+          : new MeterProvider({
+              // This is the default meter provider
+              // Create a resource. Fill the `service.*` attributes in with real values for your service.
+              // GcpDetectorSync will add in resource information about the current environment if you are
+              // running on GCP. These resource attributes will be translated to a specific GCP monitored
+              // resource if running on GCP. Otherwise, metrics will be sent with monitored resource
+              // `generic_task`.
+              resource: new Resources.Resource({
+                'service.name': 'bigtable-metrics',
+              }).merge(new ResourceUtil.GcpDetectorSync().detect()),
+              readers: [
+                // Register the exporter
+                new PeriodicExportingMetricReader({
+                  // Export metrics every 10 seconds. 5 seconds is the smallest sample period allowed by
+                  // Cloud Monitoring.
+                  exportIntervalMillis: 10_000,
+                  exporter: new MetricExporter({
+                    projectId,
+                  }),
                 }),
-              }),
-            ],
-          });
-    const meter = meterProvider.getMeter('bigtable.googleapis.com');
-    this.metrics = {
-      operationLatencies: meter.createHistogram('operation_latencies', {
-        description:
-          "The total end-to-end latency across all RPC attempts associated with a Bigtable operation. This metric measures an operation's round trip from the client to Bigtable and back to the client and includes all retries.",
-      }),
-      attemptLatencies: meter.createHistogram('attempt_latencies', {
-        description:
-          'The latencies of a client RPC attempt. Under normal circumstances, this value is identical to operation_latencies. If the client receives transient errors, however, then operation_latencies is the sum of all attempt_latencies and the exponential delays.',
-        unit: 'ms',
-      }),
-      retryCount: meter.createCounter('retry_count', {
-        description:
-          'A counter that records the number of attempts that an operation required to complete. Under normal circumstances, this value is empty.',
-      }),
-      applicationBlockingLatencies: meter.createHistogram(
-        'application_blocking_latencies',
-        {
+              ],
+            });
+      const meter = meterProvider.getMeter('bigtable.googleapis.com');
+      this.metrics = {
+        operationLatencies: meter.createHistogram('operation_latencies', {
           description:
-            'The time from when the client receives the response to a request until the application reads the response. This metric is most relevant for ReadRows requests. The start and stop times for this metric depend on the way that you send the read request; see Application blocking latencies timer examples for details.',
+            "The total end-to-end latency across all RPC attempts associated with a Bigtable operation. This metric measures an operation's round trip from the client to Bigtable and back to the client and includes all retries.",
+        }),
+        attemptLatencies: meter.createHistogram('attempt_latencies', {
+          description:
+            'The latencies of a client RPC attempt. Under normal circumstances, this value is identical to operation_latencies. If the client receives transient errors, however, then operation_latencies is the sum of all attempt_latencies and the exponential delays.',
           unit: 'ms',
-        }
-      ),
-      firstResponseLatencies: meter.createHistogram(
-        'first_response_latencies',
-        {
+        }),
+        retryCount: meter.createCounter('retry_count', {
           description:
-            'Latencies from when a client sends a request and receives the first row of the response.',
-          unit: 'ms',
-        }
-      ),
-      serverLatencies: meter.createHistogram('server_latencies', {
-        description:
-          'Latencies between the time when the Google frontend receives an RPC and when it sends the first byte of the response.',
-      }),
-      connectivityErrorCount: meter.createHistogram(
-        'connectivity_error_count',
-        {
+            'A counter that records the number of attempts that an operation required to complete. Under normal circumstances, this value is empty.',
+        }),
+        applicationBlockingLatencies: meter.createHistogram(
+          'application_blocking_latencies',
+          {
+            description:
+              'The time from when the client receives the response to a request until the application reads the response. This metric is most relevant for ReadRows requests. The start and stop times for this metric depend on the way that you send the read request; see Application blocking latencies timer examples for details.',
+            unit: 'ms',
+          }
+        ),
+        firstResponseLatencies: meter.createHistogram(
+          'first_response_latencies',
+          {
+            description:
+              'Latencies from when a client sends a request and receives the first row of the response.',
+            unit: 'ms',
+          }
+        ),
+        serverLatencies: meter.createHistogram('server_latencies', {
           description:
-            "The number of requests that failed to reach Google's network. In normal cases, this number is 0. When the number is not 0, it can indicate connectivity issues between the application and the Google network.",
-        }
-      ),
-      clientBlockingLatencies: meter.createHistogram(
-        'client_blocking_latencies',
-        {
-          description:
-            'Latencies introduced when the client blocks the sending of more requests to the server because of too many pending requests in a bulk operation.',
-          unit: 'ms',
-        }
-      ),
-    };
+            'Latencies between the time when the Google frontend receives an RPC and when it sends the first byte of the response.',
+        }),
+        connectivityErrorCount: meter.createHistogram(
+          'connectivity_error_count',
+          {
+            description:
+              "The number of requests that failed to reach Google's network. In normal cases, this number is 0. When the number is not 0, it can indicate connectivity issues between the application and the Google network.",
+          }
+        ),
+        clientBlockingLatencies: meter.createHistogram(
+          'client_blocking_latencies',
+          {
+            description:
+              'Latencies introduced when the client blocks the sending of more requests to the server because of too many pending requests in a bulk operation.',
+            unit: 'ms',
+          }
+        ),
+      };
+      return this.metrics;
+    }
   }
 
   /**
@@ -569,15 +582,18 @@ export class MetricsTracerFactory {
    * @param tabularApiSurface The Bigtable table being accessed.
    * @param methodName The name of the method being traced.
    * @param dateProvider An optional DateProvider for testing purposes.
+   * @param {string} projectId The project id
    * @returns A new MetricsTracer instance.
    */
   getMetricsTracer(
     tabularApiSurface: ITabularApiSurface,
     methodName: string,
+    projectId?: string,
     dateProvider?: DateProvider
   ) {
+    const metrics = this.initialize(projectId, this.observabilityOptions);
     return new MetricsTracer(
-      this.metrics,
+      metrics,
       tabularApiSurface,
       methodName,
       dateProvider
--- test/metrics-tracer/metrics-tracer.ts
+++ test/metrics-tracer/metrics-tracer.ts
@@ -82,7 +82,7 @@ class FakeInstance {
 }
 
 describe.only('Bigtable/MetricsTracer', () => {
-  it('should record the right metrics with a typical method call', () => {
+  it('should record the right metrics with a typical method call', async () => {
     const logger = new Logger();
     class FakeTable {
       id = 'fakeTableId';
@@ -91,101 +91,109 @@ describe.only('Bigtable/MetricsTracer', () => {
         meterProvider: new TestMeterProvider(logger),
       });
 
-      fakeMethod(): void {
-        const standardAttemptInfo = {
-          finalOperationStatus: 'PENDING',
-          streamingOperation: 'YES',
-        };
-        function createMetadata(duration: string) {
-          return {
-            internalRepr: new Map([
-              ['server-timing', Buffer.from(`gfet4t7; dur=${duration}`)],
-            ]),
-            options: {},
-          };
-        }
-        const status = {
-          metadata: {
-            internalRepr: new Map([
-              [
-                'x-goog-ext-425905942-bin',
-                Buffer.from('\n\nus-west1-c \rfake-cluster3'),
-              ],
-            ]),
-            options: {},
-          },
-        };
-        const metricsTracer =
-          this.bigtable.metricsTracerFactory.getMetricsTracer(
-            this,
-            'fakeMethod',
-            new TestDateProvider(logger)
-          );
-        // In this method we simulate a series of events that might happen
-        // when a user calls one of the Table methods.
-        // Here is an example of what might happen in a method call:
-        logger.log('1. The operation starts');
-        metricsTracer.onOperationStart();
-        logger.log('2. The attempt starts.');
-        metricsTracer.onAttemptStart();
-        logger.log('3. Client receives status information.');
-        metricsTracer.onStatusReceived(status);
-        logger.log('4. Client receives metadata.');
-        metricsTracer.onMetadataReceived(
-          standardAttemptInfo,
-          createMetadata('101')
-        );
-        logger.log('5. Client receives first row.');
-        metricsTracer.onResponse('PENDING');
-        logger.log('6. Client receives metadata.');
-        metricsTracer.onMetadataReceived(
-          standardAttemptInfo,
-          createMetadata('102')
-        );
-        logger.log('7. Client receives second row.');
-        metricsTracer.onResponse('PENDING');
-        logger.log('8. A transient error occurs.');
-        metricsTracer.onAttemptComplete({
-          finalOperationStatus: 'ERROR',
-          streamingOperation: 'YES',
-        });
-        logger.log('9. After a timeout, the second attempt is made.');
-        metricsTracer.onAttemptStart();
-        logger.log('10. Client receives status information.');
-        metricsTracer.onStatusReceived(status);
-        logger.log('11. Client receives metadata.');
-        metricsTracer.onMetadataReceived(
-          standardAttemptInfo,
-          createMetadata('103')
-        );
-        logger.log('12. Client receives third row.');
-        metricsTracer.onResponse('PENDING');
-        logger.log('13. Client receives metadata.');
-        metricsTracer.onMetadataReceived(
-          {finalOperationStatus: 'PENDING', streamingOperation: 'YES'},
-          createMetadata('104')
-        );
-        logger.log('14. Client receives fourth row.');
-        metricsTracer.onResponse('PENDING');
-        logger.log('15. User reads row 1');
-        metricsTracer.onRead();
-        logger.log('16. User reads row 2');
-        metricsTracer.onRead();
-        logger.log('17. User reads row 3');
-        metricsTracer.onRead();
-        logger.log('18. User reads row 4');
-        metricsTracer.onRead();
-        logger.log('19. Stream ends, operation completes');
-        metricsTracer.onOperationComplete({
-          retries: 1,
-          finalOperationStatus: 'SUCCESS',
-          connectivityErrorCount: 1,
-          streamingOperation: 'YES',
+      async fakeMethod(): Promise<void> {
+        return new Promise((resolve, reject) => {
+          this.bigtable.getProjectId_((err, projectId) => {
+            const standardAttemptInfo = {
+              finalOperationStatus: 'PENDING',
+              streamingOperation: 'YES',
+            };
+
+            function createMetadata(duration: string) {
+              return {
+                internalRepr: new Map([
+                  ['server-timing', Buffer.from(`gfet4t7; dur=${duration}`)],
+                ]),
+                options: {},
+              };
+            }
+
+            const status = {
+              metadata: {
+                internalRepr: new Map([
+                  [
+                    'x-goog-ext-425905942-bin',
+                    Buffer.from('\n\nus-west1-c \rfake-cluster3'),
+                  ],
+                ]),
+                options: {},
+              },
+            };
+            const metricsTracer =
+              this.bigtable.metricsTracerFactory.getMetricsTracer(
+                this,
+                'fakeMethod',
+                projectId,
+                new TestDateProvider(logger)
+              );
+            // In this method we simulate a series of events that might happen
+            // when a user calls one of the Table methods.
+            // Here is an example of what might happen in a method call:
+            logger.log('1. The operation starts');
+            metricsTracer.onOperationStart();
+            logger.log('2. The attempt starts.');
+            metricsTracer.onAttemptStart();
+            logger.log('3. Client receives status information.');
+            metricsTracer.onStatusReceived(status);
+            logger.log('4. Client receives metadata.');
+            metricsTracer.onMetadataReceived(
+              standardAttemptInfo,
+              createMetadata('101')
+            );
+            logger.log('5. Client receives first row.');
+            metricsTracer.onResponse('PENDING');
+            logger.log('6. Client receives metadata.');
+            metricsTracer.onMetadataReceived(
+              standardAttemptInfo,
+              createMetadata('102')
+            );
+            logger.log('7. Client receives second row.');
+            metricsTracer.onResponse('PENDING');
+            logger.log('8. A transient error occurs.');
+            metricsTracer.onAttemptComplete({
+              finalOperationStatus: 'ERROR',
+              streamingOperation: 'YES',
+            });
+            logger.log('9. After a timeout, the second attempt is made.');
+            metricsTracer.onAttemptStart();
+            logger.log('10. Client receives status information.');
+            metricsTracer.onStatusReceived(status);
+            logger.log('11. Client receives metadata.');
+            metricsTracer.onMetadataReceived(
+              standardAttemptInfo,
+              createMetadata('103')
+            );
+            logger.log('12. Client receives third row.');
+            metricsTracer.onResponse('PENDING');
+            logger.log('13. Client receives metadata.');
+            metricsTracer.onMetadataReceived(
+              {finalOperationStatus: 'PENDING', streamingOperation: 'YES'},
+              createMetadata('104')
+            );
+            logger.log('14. Client receives fourth row.');
+            metricsTracer.onResponse('PENDING');
+            logger.log('15. User reads row 1');
+            metricsTracer.onRead();
+            logger.log('16. User reads row 2');
+            metricsTracer.onRead();
+            logger.log('17. User reads row 3');
+            metricsTracer.onRead();
+            logger.log('18. User reads row 4');
+            metricsTracer.onRead();
+            logger.log('19. Stream ends, operation completes');
+            metricsTracer.onOperationComplete({
+              retries: 1,
+              finalOperationStatus: 'SUCCESS',
+              connectivityErrorCount: 1,
+              streamingOperation: 'YES',
+            });
+            resolve();
+          });
         });
       }
     }
     const table = new FakeTable();
-    table.fakeMethod();
+    await table.fakeMethod();
     const expectedOutput = fs.readFileSync(
       './test/metrics-tracer/typical-method-call.txt',
       'utf8'
