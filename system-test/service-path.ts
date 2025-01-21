cea3b8d920b70d5ee87f2d0bd511bb9e772882ca
Daniel Bruce <djbruce@google.com>
2025-01-21 16:36:25 -0500

    Update from test comments about environment variab

--- system-test/service-path.ts
+++ system-test/service-path.ts
@@ -20,6 +20,9 @@ import {BigtableClient, BigtableInstanceAdminClient} from '../src/v2';
 
 describe('Service Path', () => {
   it('Setting universe domain should set the service path', async () => {
+    // Set the environment variable
+    process.env.GOOGLE_CLOUD_UNIVERSE_DOMAIN = 'otherDomain';
+
     const universeDomain = 'someUniverseDomain'; // or your universe domain if not using emulator
     const options = {
       universeDomain,
@@ -70,8 +73,14 @@ describe('Service Path', () => {
         `bigtable.${universeDomain}`
       );
     }
+
+    // Clean up the environment variable after the test
+    delete process.env.GOOGLE_CLOUD_UNIVERSE_DOMAIN;
   });
   it('Setting universe domain and custom endpoint should set the service path to custom endpoint', async () => {
+    // Set the environment variable
+    process.env.GOOGLE_CLOUD_UNIVERSE_DOMAIN = 'otherDomain';
+
     const universeDomain = 'someUniverseDomain'; // or your universe domain if not using emulator
     const apiEndpoint = 'someApiEndpoint';
     const options = {
@@ -124,5 +133,62 @@ describe('Service Path', () => {
         apiEndpoint
       );
     }
+
+    // Clean up the environment variable after the test
+    delete process.env.GOOGLE_CLOUD_UNIVERSE_DOMAIN;
+  });
+  it('Setting GOOGLE_CLOUD_UNIVERSE_DOMAIN should set the service path', async () => {
+    const universeDomain = 'someUniverseDomain'; // or your universe domain if not using emulator
+
+    // Set the environment variable
+    process.env.GOOGLE_CLOUD_UNIVERSE_DOMAIN = universeDomain;
+    const bigtable = new Bigtable(); // No options needed, it will pick up the env var
+
+    // Need to mock getProjectId_ since it normally uses auth and auth isn't
+    // available in unit tests.
+    bigtable.getProjectId_ = (
+      callback: (err: Error | null, projectId?: string) => void
+    ) => {
+      callback(null, 'projectId');
+    };
+
+    try {
+      // This is necessary to initialize the bigtable instance admin client.
+      await bigtable.getInstances({timeout: 1000});
+    } catch (e) {
+      assert.strictEqual(
+        (e as ServiceError).message,
+        'Total timeout of API google.bigtable.admin.v2.BigtableInstanceAdmin exceeded 1000 milliseconds retrying error Error: 14 UNAVAILABLE: Name resolution failed for target dns:bigtableadmin.someUniverseDomain:443  before any response was received.'
+      );
+    } finally {
+      assert.strictEqual(
+        (
+          bigtable.api
+            .BigtableInstanceAdminClient as BigtableInstanceAdminClient
+        )['_opts'].servicePath,
+        `bigtableadmin.${universeDomain}`
+      );
+    }
+
+    try {
+      // This will fail in unit tests, but is necessary to initialize the
+      // bigtable client.
+      const instance = bigtable.instance('instanceId');
+      const table = instance.table('tableId');
+      await table.getRows({gaxOptions: {timeout: 1000}});
+    } catch (e) {
+      assert.strictEqual(
+        (e as ServiceError).message,
+        '14 UNAVAILABLE: Name resolution failed for target dns:bigtable.someUniverseDomain:443'
+      );
+    } finally {
+      assert.strictEqual(
+        (bigtable.api.BigtableClient as BigtableClient)['_opts'].servicePath,
+        `bigtable.${universeDomain}`
+      );
+    }
+
+    // Clean up the environment variable after the test
+    delete process.env.GOOGLE_CLOUD_UNIVERSE_DOMAIN;
   });
 });
