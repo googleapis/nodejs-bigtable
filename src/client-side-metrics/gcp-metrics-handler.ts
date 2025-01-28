efdfcead853a7331e66eefd9884de8d9cb08ddc0
Daniel Bruce <djbruce@google.com>
2025-01-28 10:40:59 -0500

    Remove copy/pasted comment

--- src/client-side-metrics/gcp-metrics-handler.ts
+++ src/client-side-metrics/gcp-metrics-handler.ts
@@ -67,12 +67,6 @@ export class GCPMetricsHandler implements IMetricsHandler {
       // Use MeterProvider provided by user
       // If MeterProvider was not provided then use the default meter provider.
       const meterProvider = new MeterProvider({
-        // This is the default meter provider
-        // Create a resource. Fill the `service.*` attributes in with real values for your service.
-        // GcpDetectorSync will add in resource information about the current environment if you are
-        // running on GCP. These resource attributes will be translated to a specific GCP monitored
-        // resource if running on GCP. Otherwise, metrics will be sent with monitored resource
-        // `generic_task`.
         resource: new Resources.Resource({
           'service.name': 'bigtable-metrics',
         }).merge(new ResourceUtil.GcpDetectorSync().detect()),
