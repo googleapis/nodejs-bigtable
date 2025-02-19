801d9354f7138899f21a22fb0fa745898a47e0af
Daniel Bruce <djbruce@google.com>
2025-02-19 10:52:44 -0500

    More serializer experiments

--- test/nested/serializing.ts
+++ test/nested/serializing.ts
@@ -4,6 +4,7 @@ const root = gax.protobuf.loadSync(
   './protos/google/bigtable/v2/response_params.proto'
 );
 const ResponseParams = root.lookupType('ResponseParams');
+import {serializer} from 'google-gax';
 const status = {
   metadata: {
     internalRepr: new Map([
@@ -18,17 +19,27 @@ const status = {
 
 describe.only('Bigtable/Table', () => {
   it('decode', () => {
+    const buffer = new TextEncoder().encode('\n\nus-west1-c \rfake-cluster3');
     // const result3 = gax.protobuf.parse('\n\nus-west1-c \rfake-cluster3');
+    const encoded = ResponseParams.encode(buffer);
+    const something = ResponseParams.oneofsArray;
     const result4 = ResponseParams.decodeDelimited(
       Buffer.from('\n\nus-west1-c \rfake-cluster3')
     );
     // ResponseParams.toObject(Buffer.from('\n\nus-west1-c \rfake-cluster3'))
     const result2 = ResponseParams.get('\n\nus-west1-c \rfake-cluster3');
-    /*
+
     const result = ResponseParams.decode(
+      buffer, // Buffer.from('\n\nus-west1-c \rfake-cluster3'),
+      2
+    );
+    const result5 = serializer.toProto3JSON(result4);
+    const result6 = serializer.fromProto3JSON(
+      ResponseParams,
       Buffer.from('\n\nus-west1-c \rfake-cluster3')
     );
-     */
     console.log(result4);
+    console.log(result);
+    console.log(result5);
   });
 });
