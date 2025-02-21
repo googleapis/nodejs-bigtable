eb8f14b95cf7aee72cee8b20fbc5f2d2145f321e
Daniel Bruce <djbruce@google.com>
2025-02-21 14:33:11 -0500

    Eliminate tests using the old export input fixture

--- system-test/cloud-monitoring-exporter.ts
+++ system-test/cloud-monitoring-exporter.ts
@@ -3,11 +3,7 @@ import {
   CloudMonitoringExporter,
   ExportResult,
 } from '../src/client-side-metrics/exporter';
-import {
-  exportInput,
-  fakeEndTime,
-  fakeStartTime,
-} from '../test-common/export-input-fixture';
+import {fakeEndTime, fakeStartTime} from '../test-common/export-input-fixture';
 import {ResourceMetrics} from '@opentelemetry/sdk-metrics';
 import {Bigtable} from '../src';
 import * as assert from 'assert';
@@ -15,38 +11,10 @@ import {expectedOtelExportInput} from '../test-common/expected-otel-export-input
 import {replaceTimestamps} from '../test-common/replace-timestamps';
 
 describe('Bigtable/CloudMonitoringExporter', () => {
-  it('exports client side metrics to cloud monitoring', done => {
+  it('Should send an otel exported value to the CloudMonitoringExporter', done => {
     // When this test is run, metrics should be visible at the following link:
     // https://pantheon.corp.google.com/monitoring/metrics-explorer;duration=PT1H?inv=1&invt=Abo9_A&project={projectId}
     // This test will add metrics so that they are available in Pantheon
-    (async () => {
-      const bigtable = new Bigtable();
-      const projectId: string = await new Promise((resolve, reject) => {
-        bigtable.getProjectId_((err, projectId) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(projectId as string);
-          }
-        });
-      });
-      const transformedExportInput = JSON.parse(
-        JSON.stringify(exportInput).replace(/some-project/g, projectId)
-      );
-      const exporter = new CloudMonitoringExporter();
-      exporter.export(
-        transformedExportInput as unknown as ResourceMetrics,
-        (result: {code: number}) => {
-          if (result.code === 0) {
-            done();
-          } else {
-            done(result.code);
-          }
-        }
-      );
-    })();
-  });
-  it.only('Should send an otel exported value to the CloudMonitoringExporter', done => {
     (async () => {
       const resultCallback: (result: ExportResult) => void = (
         result: ExportResult
