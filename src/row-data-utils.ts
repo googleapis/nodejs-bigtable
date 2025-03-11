55880b66d6b61080173a94a2379e0a35528a4be5
Daniel Bruce <djbruce@google.com>
2025-03-11 17:41:55 -0400

    Drop dotProp

--- src/row-data-utils.ts
+++ src/row-data-utils.ts
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import * as dotProp from 'dot-prop';
 import {Filter, RawFilter} from './filter';
 import {
   CreateRulesCallback,
