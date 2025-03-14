35da5c6b27748508211b11b057ec7301fc6abaa4
Daniel Bruce <djbruce@google.com>
2025-03-14 10:32:39 -0400

    Completed the test for two metrics handlers

--- system-test/gcp-metrics-handler.ts
+++ system-test/gcp-metrics-handler.ts
@@ -21,11 +21,37 @@ import {
 } from '../src/client-side-metrics/metrics-handler';
 import {
   CloudMonitoringExporter,
+  ExportInput,
   ExportResult,
 } from '../src/client-side-metrics/exporter';
 import {Bigtable} from '../src';
 import {ResourceMetrics} from '@opentelemetry/sdk-metrics';
 import * as assert from 'assert';
+import {expectedOtelExportInput} from '../test-common/expected-otel-export-input';
+
+/**
+ * Replaces the timestamp values within an `ExportInput` object with
+ * standardized test values.
+ *
+ * This function is designed for testing purposes to make timestamp comparisons
+ * in tests more predictable and reliable. It recursively traverses the
+ * `ExportInput` object, finds all `startTime` and `endTime` properties, and
+ * replaces their numeric values with standardized test values.
+ */
+function replaceTimestamps(
+  request: typeof expectedOtelExportInput,
+  newStartTime: [number, number],
+  newEndTime: [number, number]
+) {
+  request.scopeMetrics.forEach(scopeMetric => {
+    scopeMetric.metrics.forEach(metric => {
+      metric.dataPoints.forEach(dataPoint => {
+        dataPoint.startTime = newStartTime;
+        dataPoint.endTime = newEndTime;
+      });
+    });
+  });
+}
 
 describe('Bigtable/GCPMetricsHandler', () => {
   it('Should export a value to the GCPMetricsHandler', done => {
@@ -99,7 +125,7 @@ describe('Bigtable/GCPMetricsHandler', () => {
       }
     })();
   });
-  it('Should export a value to two GCPMetricsHandlers', done => {
+  it.only('Should export a value to two GCPMetricsHandlers', done => {
     // This test ensures that when we create two GCPMetricsHandlers much like
     // what we would be doing when calling readRows on two separate tables that
     // the data doesn't store duplicates in the same place and export twice as
@@ -116,33 +142,81 @@ describe('Bigtable/GCPMetricsHandler', () => {
       it receives once. Since done cannot be called multiple times in mocha,
       exported variable ensures we only test the value export receives one time.
       */
-      let exported = false;
+      let exportedCount = 0;
       function getTestResultCallback(
-          resultCallback: (result: ExportResult) => void
+        resultCallback: (result: ExportResult) => void
       ) {
         return (result: ExportResult) => {
-          if (!exported) {
-            exported = true;
+          if (exportedCount < 2) {
+            exportedCount++;
             try {
-              clearTimeout(timeout);
               assert.strictEqual(result.code, 0);
-              done();
-              resultCallback({code: 0});
             } catch (error) {
               // Code isn't 0 so report the original error.
               done(result);
               done(error);
             }
+            if (exportedCount === 2) {
+              // We are expecting two calls to an exporter. One for each
+              // metrics handler.
+              clearTimeout(timeout);
+              done();
+            }
+            resultCallback({code: 0});
           } else {
+            // After the test is complete the periodic exporter may still be
+            // running in which case we don't want to do any checks. We just
+            // want to call the resultCallback so that there are no hanging
+            // threads.
             resultCallback({code: 0});
           }
         };
       }
       class MockExporter extends CloudMonitoringExporter {
         export(
-            metrics: ResourceMetrics,
-            resultCallback: (result: ExportResult) => void
+          metrics: ResourceMetrics,
+          resultCallback: (result: ExportResult) => void
         ): void {
+          try {
+            // The code block ensures the metrics are correct. Mainly, the metrics
+            // shouldn't contain two copies of the data. It should only contain
+            // one.
+
+            // For this test since we are still writing a time series with
+            // metrics variable we don't want to modify the metrics variable
+            // to have artificial times because then sending the data to the
+            // metric service client will fail. Therefore, we must make a copy
+            // of the metrics and use that.
+            const parsedExportInput = JSON.parse(JSON.stringify(metrics));
+            replaceTimestamps(
+              parsedExportInput as unknown as typeof expectedOtelExportInput,
+              [123, 789],
+              [456, 789]
+            );
+            assert.deepStrictEqual(
+              (parsedExportInput as ExportInput).scopeMetrics[0].metrics.length,
+              expectedOtelExportInput.scopeMetrics[0].metrics.length
+            );
+            for (
+              let index = 0;
+              index <
+              (parsedExportInput as ExportInput).scopeMetrics[0].metrics.length;
+              index++
+            ) {
+              // We need to compare pointwise because mocha truncates to an 8192 character limit.
+              assert.deepStrictEqual(
+                (parsedExportInput as ExportInput).scopeMetrics[0].metrics[
+                  index
+                ],
+                expectedOtelExportInput.scopeMetrics[0].metrics[index]
+              );
+            }
+          } catch (e) {
+            // The error needs to be caught so it can be reported to the mocha
+            // test runner.
+            done(e);
+          }
+          // The code below uses the test callback to ensure the export was successful.
           const testResultCallback = getTestResultCallback(resultCallback);
           super.export(metrics, testResultCallback);
         }
@@ -159,17 +233,25 @@ describe('Bigtable/GCPMetricsHandler', () => {
         });
       });
       const handler = new GCPMetricsHandler(new MockExporter({projectId}));
+      const handler2 = new GCPMetricsHandler(new MockExporter({projectId}));
       const transformedRequestsHandled = JSON.parse(
-          JSON.stringify(expectedRequestsHandled).replace(
-              /my-project/g,
-              projectId
-          )
+        JSON.stringify(expectedRequestsHandled).replace(
+          /my-project/g,
+          projectId
+        )
       );
       for (const request of transformedRequestsHandled) {
         if (request.attemptLatency) {
           handler.onAttemptComplete(request as OnAttemptCompleteData);
         } else {
           handler.onOperationComplete(request as OnOperationCompleteData);
+        }
+      }
+      for (const request of transformedRequestsHandled) {
+        if (request.attemptLatency) {
+          handler2.onAttemptComplete(request as OnAttemptCompleteData);
+        } else {
+          handler2.onOperationComplete(request as OnOperationCompleteData);
         }
       }
     })();
