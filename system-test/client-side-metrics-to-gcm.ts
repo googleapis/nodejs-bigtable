3b71eff2225a63ec2e8a635ddf5e69ec9bb9b9ec
Daniel Bruce <djbruce@google.com>
2025-03-21 16:07:44 -0400

    Eliminate TODO

--- system-test/client-side-metrics-to-gcm.ts
+++ system-test/client-side-metrics-to-gcm.ts
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// TODO: Must be put in root folder or will not run
-
 import {describe, it, before, after} from 'mocha';
 import * as assert from 'assert';
 import {Bigtable} from '../src';
