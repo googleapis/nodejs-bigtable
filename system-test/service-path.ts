3d9822aeb4aaba2156f9721f73966476ef561849
Daniel Bruce <djbruce@google.com>
2025-03-24 16:02:32 -0400

    Update the test to create an instance

--- system-test/service-path.ts
+++ system-test/service-path.ts
@@ -195,6 +195,39 @@ describe('Service Path', () => {
 
 describe.only('Service Path2', () => {
   it('Experiment with setting the service path', async () => {
+    const instanceId = 'instanceId';
+    const tableId = 'tableId';
+    const columnFamilyId = 'cf1';
+    async function mockBigtable() {
+      const instance = bigtable.instance(instanceId);
+      const [instanceInfo] = await instance.exists();
+      if (!instanceInfo) {
+        const [, operation] = await instance.create({
+          clusters: {
+            id: 'fake-cluster3',
+            location: 'us-west1-c',
+            nodes: 1,
+          },
+        });
+        await operation.promise();
+      }
+
+      const table = instance.table(tableId);
+      const [tableExists] = await table.exists();
+      if (!tableExists) {
+        await table.create({families: [columnFamilyId]}); // Create column family
+      } else {
+        // Check if column family exists and create it if not.
+        const [families] = await table.getFamilies();
+        if (
+          !families.some((family: {id: string}) => family.id === columnFamilyId)
+        ) {
+          await table.createFamily(columnFamilyId);
+        }
+      }
+    }
+    mockBigtable();
+    // Do the universe domain test
     const universeDomain = 'apis-tpczero.goog'; // or your universe domain if not using emulator
     const options = {
       universeDomain,
@@ -204,8 +237,8 @@ describe.only('Service Path2', () => {
       BigtableInstanceAdminClient: options,
       BigtableTableAdminClient: options,
     });
-    const instance = bigtable.instance('instanceId');
-    const table = instance.table('tableId');
+    const instance = bigtable.instance(instanceId);
+    const table = instance.table(tableId);
     await table.getRows({gaxOptions: {timeout: 1000}});
   });
 });
