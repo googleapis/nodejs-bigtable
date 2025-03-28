f2d16bd3445590f58c73714cd1677156e7e9dbee
Daniel Bruce <djbruce@google.com>
2025-03-28 17:27:46 -0400

    Add the optimizations back again

--- src/client-side-metrics/operation-metrics-collector.ts
+++ src/client-side-metrics/operation-metrics-collector.ts
@@ -207,16 +207,18 @@ export class OperationMetricsCollector {
    * Called when the first response is received. Records first response latencies.
    */
   onResponse(projectId: string) {
-    if (
-      this.state ===
-      MetricsCollectorState.OPERATION_STARTED_ATTEMPT_IN_PROGRESS_NO_ROWS_YET
-    ) {
-      this.state =
-        MetricsCollectorState.OPERATION_STARTED_ATTEMPT_IN_PROGRESS_SOME_ROWS_RECEIVED;
-      const endTime = new Date();
-      if (projectId && this.operationStartTime) {
-        this.firstResponseLatency =
-          endTime.getTime() - this.operationStartTime.getTime();
+    if (!this.firstResponseLatency) {
+      if (
+        this.state ===
+        MetricsCollectorState.OPERATION_STARTED_ATTEMPT_IN_PROGRESS_NO_ROWS_YET
+      ) {
+        this.state =
+          MetricsCollectorState.OPERATION_STARTED_ATTEMPT_IN_PROGRESS_SOME_ROWS_RECEIVED;
+        const endTime = new Date();
+        if (projectId && this.operationStartTime) {
+          this.firstResponseLatency =
+            endTime.getTime() - this.operationStartTime.getTime();
+        }
       }
     }
   }
@@ -266,26 +268,29 @@ export class OperationMetricsCollector {
     internalRepr: Map<string, string[]>;
     options: {};
   }) {
-    const mappedEntries = new Map(
-      Array.from(metadata.internalRepr.entries(), ([key, value]) => [
-        key,
-        value.toString(),
-      ])
-    );
-    const SERVER_TIMING_REGEX = /.*gfet4t7;\s*dur=(\d+\.?\d*).*/;
-    const SERVER_TIMING_KEY = 'server-timing';
-    const durationValues = mappedEntries.get(SERVER_TIMING_KEY);
-    if (durationValues) {
-      const matchedDuration = durationValues?.match(SERVER_TIMING_REGEX);
-      if (matchedDuration && matchedDuration[1]) {
-        if (!this.serverTimeRead) {
-          this.serverTimeRead = true;
-          this.serverTime = isNaN(parseInt(matchedDuration[1]))
-            ? null
-            : parseInt(matchedDuration[1]);
+    if (!this.serverTimeRead && this.connectivityErrorCount < 1) {
+      // Check serverTimeRead, connectivityErrorCount here to reduce latency.
+      const mappedEntries = new Map(
+        Array.from(metadata.internalRepr.entries(), ([key, value]) => [
+          key,
+          value.toString(),
+        ])
+      );
+      const SERVER_TIMING_REGEX = /.*gfet4t7;\s*dur=(\d+\.?\d*).*/;
+      const SERVER_TIMING_KEY = 'server-timing';
+      const durationValues = mappedEntries.get(SERVER_TIMING_KEY);
+      if (durationValues) {
+        const matchedDuration = durationValues?.match(SERVER_TIMING_REGEX);
+        if (matchedDuration && matchedDuration[1]) {
+          if (!this.serverTimeRead) {
+            this.serverTimeRead = true;
+            this.serverTime = isNaN(parseInt(matchedDuration[1]))
+              ? null
+              : parseInt(matchedDuration[1]);
+          }
+        } else {
+          this.connectivityErrorCount = 1;
         }
-      } else {
-        this.connectivityErrorCount = 1;
       }
     }
   }
@@ -297,28 +302,30 @@ export class OperationMetricsCollector {
   onStatusMetadataReceived(status: {
     metadata: {internalRepr: Map<string, Uint8Array[]>; options: {}};
   }) {
-    const INSTANCE_INFORMATION_KEY = 'x-goog-ext-425905942-bin';
-    const mappedValue = status.metadata.internalRepr.get(
-      INSTANCE_INFORMATION_KEY
-    ) as Buffer[];
-    if (mappedValue && mappedValue[0] && ResponseParams) {
-      const decodedValue = ResponseParams.decode(
-        mappedValue[0],
-        mappedValue[0].length
-      );
-      if (
-        decodedValue &&
-        (decodedValue as unknown as {zoneId: string}).zoneId
-      ) {
-        this.zone = (decodedValue as unknown as {zoneId: string}).zoneId;
-      }
-      if (
-        decodedValue &&
-        (decodedValue as unknown as {clusterId: string}).clusterId
-      ) {
-        this.cluster = (
-          decodedValue as unknown as {clusterId: string}
-        ).clusterId;
+    if (!this.zone || !this.cluster) {
+      const INSTANCE_INFORMATION_KEY = 'x-goog-ext-425905942-bin';
+      const mappedValue = status.metadata.internalRepr.get(
+        INSTANCE_INFORMATION_KEY
+      ) as Buffer[];
+      if (mappedValue && mappedValue[0] && ResponseParams) {
+        const decodedValue = ResponseParams.decode(
+          mappedValue[0],
+          mappedValue[0].length
+        );
+        if (
+          decodedValue &&
+          (decodedValue as unknown as {zoneId: string}).zoneId
+        ) {
+          this.zone = (decodedValue as unknown as {zoneId: string}).zoneId;
+        }
+        if (
+          decodedValue &&
+          (decodedValue as unknown as {clusterId: string}).clusterId
+        ) {
+          this.cluster = (
+            decodedValue as unknown as {clusterId: string}
+          ).clusterId;
+        }
       }
     }
   }
