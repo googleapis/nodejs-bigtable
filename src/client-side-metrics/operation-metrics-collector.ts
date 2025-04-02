e243c115a23ac4740b4726f42b1aecd2c83d9394
Daniel Bruce <djbruce@google.com>
2025-04-02 09:40:04 -0400

    Revert "Remove response params"
    This reverts commit 6d9551cd1d8c4bc3ecb4c7530e51001854643530.

--- src/client-side-metrics/operation-metrics-collector.ts
+++ src/client-side-metrics/operation-metrics-collector.ts
@@ -18,10 +18,22 @@ import {MethodName, StreamingState} from './client-side-metrics-attributes';
 import {grpc} from 'google-gax';
 import * as gax from 'google-gax';
 
-const root = gax.protobuf.loadSync(
-  './protos/google/bigtable/v2/response_params.proto'
-);
-const ResponseParams = root.lookupType('ResponseParams');
+let ResponseParams: gax.protobuf.Type | null;
+try {
+  /*
+   * Likely due to the Node 18 upgrade, the samples tests are failing with the
+   * error UnhandledPromiseRejectionWarning: Error: ENOENT: no such file or
+   * directory, open 'protos/google/bigtable/v2/response_params.proto'. Since
+   * these tests don't use this module we can suppress the error for now to
+   * unblock the CI pipeline.
+   */
+  const root = gax.protobuf.loadSync(
+    './protos/google/bigtable/v2/response_params.proto'
+  );
+  ResponseParams = root.lookupType('ResponseParams');
+} catch (e) {
+  ResponseParams = null;
+}
 
 /**
  * An interface representing a tabular API surface, such as a Bigtable table.
