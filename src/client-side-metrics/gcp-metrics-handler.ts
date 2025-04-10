fe8e9ef8f5ff8569f59b684bfaff16bed751ab98
Daniel Bruce <djbruce@google.com>
2025-04-10 16:27:22 -0400

    Pull getInstruments function out

--- src/client-side-metrics/gcp-metrics-handler.ts
+++ src/client-side-metrics/gcp-metrics-handler.ts
@@ -49,6 +49,133 @@ interface MetricsInstruments {
   clientBlockingLatencies: typeof Histogram;
 }
 
+function getInstruments(projectId: string, exporter: PushMetricExporter) {
+  const latencyBuckets = [
+    0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 8.0, 10.0, 13.0, 16.0, 20.0, 25.0,
+    30.0, 40.0, 50.0, 65.0, 80.0, 100.0, 130.0, 160.0, 200.0, 250.0, 300.0,
+    400.0, 500.0, 650.0, 800.0, 1000.0, 2000.0, 5000.0, 10000.0, 20000.0,
+    50000.0, 100000.0, 200000.0, 400000.0, 800000.0, 1600000.0, 3200000.0,
+  ];
+  const viewList = [
+    'operation_latencies',
+    'first_response_latencies',
+    'attempt_latencies',
+    'retry_count',
+    'server_latencies',
+    'connectivity_error_count',
+    'application_latencies',
+    'throttling_latencies',
+  ].map(
+      name =>
+          new View({
+            instrumentName: name,
+            name,
+            aggregation: name.endsWith('latencies')
+                ? Aggregation.Sum()
+                : new ExplicitBucketHistogramAggregation(latencyBuckets),
+          })
+  );
+  const meterProvider = new MeterProvider({
+    views: viewList,
+    resource: new Resources.Resource({
+      'service.name': 'Cloud Bigtable Table',
+      'monitored_resource.project_id': projectId,
+    }).merge(new ResourceUtil.GcpDetectorSync().detect()),
+    readers: [
+      // Register the exporter
+      new PeriodicExportingMetricReader({
+        // Export metrics every 60 seconds.
+        exportIntervalMillis: 60_000,
+        exporter,
+      }),
+    ],
+  });
+  const meter = meterProvider.getMeter('bigtable.googleapis.com');
+  return {
+    operationLatencies: meter.createHistogram(
+        'bigtable.googleapis.com/internal/client/operation_latencies',
+        {
+          description:
+              "The total end-to-end latency across all RPC attempts associated with a Bigtable operation. This metric measures an operation's round trip from the client to Bigtable and back to the client and includes all retries.",
+          unit: 'ms',
+          advice: {
+            explicitBucketBoundaries: latencyBuckets,
+          },
+        }
+    ),
+    attemptLatencies: meter.createHistogram(
+        'bigtable.googleapis.com/internal/client/attempt_latencies',
+        {
+          description:
+              'The latencies of a client RPC attempt. Under normal circumstances, this value is identical to operation_latencies. If the client receives transient errors, however, then operation_latencies is the sum of all attempt_latencies and the exponential delays.',
+          unit: 'ms',
+          advice: {
+            explicitBucketBoundaries: latencyBuckets,
+          },
+        }
+    ),
+    retryCount: meter.createCounter(
+        'bigtable.googleapis.com/internal/client/retry_count',
+        {
+          description:
+              'A counter that records the number of attempts that an operation required to complete. Under normal circumstances, this value is empty.',
+        }
+    ),
+    applicationBlockingLatencies: meter.createHistogram(
+        'bigtable.googleapis.com/internal/client/application_latencies',
+        {
+          description:
+              'The time from when the client receives the response to a request until the application reads the response. This metric is most relevant for ReadRows requests. The start and stop times for this metric depend on the way that you send the read request; see Application blocking latencies timer examples for details.',
+          unit: 'ms',
+          advice: {
+            explicitBucketBoundaries: latencyBuckets,
+          },
+        }
+    ),
+    firstResponseLatencies: meter.createHistogram(
+        'bigtable.googleapis.com/internal/client/first_response_latencies',
+        {
+          description:
+              'Latencies from when a client sends a request and receives the first row of the response.',
+          unit: 'ms',
+          advice: {
+            explicitBucketBoundaries: latencyBuckets,
+          },
+        }
+    ),
+    serverLatencies: meter.createHistogram(
+        'bigtable.googleapis.com/internal/client/server_latencies',
+        {
+          description:
+              'Latencies between the time when the Google frontend receives an RPC and when it sends the first byte of the response.',
+          unit: 'ms',
+
+          advice: {
+            explicitBucketBoundaries: latencyBuckets,
+          },
+        }
+    ),
+    connectivityErrorCount: meter.createCounter(
+        'bigtable.googleapis.com/internal/client/connectivity_error_count',
+        {
+          description:
+              "The number of requests that failed to reach Google's network. In normal cases, this number is 0. When the number is not 0, it can indicate connectivity issues between the application and the Google network.",
+        }
+    ),
+    clientBlockingLatencies: meter.createHistogram(
+        'bigtable.googleapis.com/internal/client/throttling_latencies',
+        {
+          description:
+              'Latencies introduced when the client blocks the sending of more requests to the server because of too many pending requests in a bulk operation.',
+          unit: 'ms',
+          advice: {
+            explicitBucketBoundaries: latencyBuckets,
+          },
+        }
+    ),
+  }
+}
+
 /**
  * A metrics handler implementation that uses OpenTelemetry to export metrics to Google Cloud Monitoring.
  * This handler records metrics such as operation latency, attempt latency, retry count, and more,
@@ -92,130 +219,7 @@ export class GCPMetricsHandler implements IMetricsHandler {
     // used it provides the project id for the name of the time series exported.
     // ie. name: `projects/${....['monitored_resource.project_id']}`,
     if (!this.projectToInstruments[projectId]) {
-      const latencyBuckets = [
-        0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 8.0, 10.0, 13.0, 16.0, 20.0, 25.0,
-        30.0, 40.0, 50.0, 65.0, 80.0, 100.0, 130.0, 160.0, 200.0, 250.0, 300.0,
-        400.0, 500.0, 650.0, 800.0, 1000.0, 2000.0, 5000.0, 10000.0, 20000.0,
-        50000.0, 100000.0, 200000.0, 400000.0, 800000.0, 1600000.0, 3200000.0,
-      ];
-      const viewList = [
-        'operation_latencies',
-        'first_response_latencies',
-        'attempt_latencies',
-        'retry_count',
-        'server_latencies',
-        'connectivity_error_count',
-        'application_latencies',
-        'throttling_latencies',
-      ].map(
-        name =>
-          new View({
-            instrumentName: name,
-            name,
-            aggregation: name.endsWith('latencies')
-              ? Aggregation.Sum()
-              : new ExplicitBucketHistogramAggregation(latencyBuckets),
-          })
-      );
-      const meterProvider = new MeterProvider({
-        views: viewList,
-        resource: new Resources.Resource({
-          'service.name': 'Cloud Bigtable Table',
-          'monitored_resource.project_id': projectId,
-        }).merge(new ResourceUtil.GcpDetectorSync().detect()),
-        readers: [
-          // Register the exporter
-          new PeriodicExportingMetricReader({
-            // Export metrics every 60 seconds.
-            exportIntervalMillis: 60_000,
-            exporter: this.exporter,
-          }),
-        ],
-      });
-      const meter = meterProvider.getMeter('bigtable.googleapis.com');
-      this.projectToInstruments[projectId] = {
-        operationLatencies: meter.createHistogram(
-          'bigtable.googleapis.com/internal/client/operation_latencies',
-          {
-            description:
-              "The total end-to-end latency across all RPC attempts associated with a Bigtable operation. This metric measures an operation's round trip from the client to Bigtable and back to the client and includes all retries.",
-            unit: 'ms',
-            advice: {
-              explicitBucketBoundaries: latencyBuckets,
-            },
-          }
-        ),
-        attemptLatencies: meter.createHistogram(
-          'bigtable.googleapis.com/internal/client/attempt_latencies',
-          {
-            description:
-              'The latencies of a client RPC attempt. Under normal circumstances, this value is identical to operation_latencies. If the client receives transient errors, however, then operation_latencies is the sum of all attempt_latencies and the exponential delays.',
-            unit: 'ms',
-            advice: {
-              explicitBucketBoundaries: latencyBuckets,
-            },
-          }
-        ),
-        retryCount: meter.createCounter(
-          'bigtable.googleapis.com/internal/client/retry_count',
-          {
-            description:
-              'A counter that records the number of attempts that an operation required to complete. Under normal circumstances, this value is empty.',
-          }
-        ),
-        applicationBlockingLatencies: meter.createHistogram(
-          'bigtable.googleapis.com/internal/client/application_latencies',
-          {
-            description:
-              'The time from when the client receives the response to a request until the application reads the response. This metric is most relevant for ReadRows requests. The start and stop times for this metric depend on the way that you send the read request; see Application blocking latencies timer examples for details.',
-            unit: 'ms',
-            advice: {
-              explicitBucketBoundaries: latencyBuckets,
-            },
-          }
-        ),
-        firstResponseLatencies: meter.createHistogram(
-          'bigtable.googleapis.com/internal/client/first_response_latencies',
-          {
-            description:
-              'Latencies from when a client sends a request and receives the first row of the response.',
-            unit: 'ms',
-            advice: {
-              explicitBucketBoundaries: latencyBuckets,
-            },
-          }
-        ),
-        serverLatencies: meter.createHistogram(
-          'bigtable.googleapis.com/internal/client/server_latencies',
-          {
-            description:
-              'Latencies between the time when the Google frontend receives an RPC and when it sends the first byte of the response.',
-            unit: 'ms',
-
-            advice: {
-              explicitBucketBoundaries: latencyBuckets,
-            },
-          }
-        ),
-        connectivityErrorCount: meter.createCounter(
-          'bigtable.googleapis.com/internal/client/connectivity_error_count',
-          {
-            description:
-              "The number of requests that failed to reach Google's network. In normal cases, this number is 0. When the number is not 0, it can indicate connectivity issues between the application and the Google network.",
-          }
-        ),
-        clientBlockingLatencies: meter.createHistogram(
-          'bigtable.googleapis.com/internal/client/throttling_latencies',
-          {
-            description:
-              'Latencies introduced when the client blocks the sending of more requests to the server because of too many pending requests in a bulk operation.',
-            unit: 'ms',
-            advice: {
-              explicitBucketBoundaries: latencyBuckets,
-            },
-          }
-        ),
-      };
+      this.projectToInstruments[projectId] = getInstruments(projectId, this.exporter);
     }
     return this.projectToInstruments[projectId];
   }
