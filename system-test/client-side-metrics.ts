cc32a4cfef184ab4212d48f0e14cb97acd3b4e74
Daniel Bruce <djbruce@google.com>
2025-04-11 15:40:30 -0400

    Remove only

--- system-test/client-side-metrics.ts
+++ system-test/client-side-metrics.ts
@@ -59,7 +59,7 @@ function getFakeBigtable(
   return new FakeBigtable();
 }
 
-describe.only('Bigtable/ClientSideMetrics', () => {
+describe('Bigtable/ClientSideMetrics', () => {
   const instanceId1 = 'emulator-test-instance';
   const instanceId2 = 'emulator-test-instance2';
   const tableId1 = 'my-table';
