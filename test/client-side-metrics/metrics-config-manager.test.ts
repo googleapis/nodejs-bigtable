3c1a4d0229cacd3b24350c69008d13df5b6a319b
Daniel Bruce <djbruce@google.com>
2025-05-13 11:58:02 -0400

    Address header check

--- test/client-side-metrics/metrics-config-manager.test.ts
+++ test/client-side-metrics/metrics-config-manager.test.ts
@@ -1,3 +1,17 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import * as assert from 'assert';
 import {describe, it, beforeEach, afterEach} from 'mocha';
 import * as sinon from 'sinon';
@@ -5,7 +19,6 @@ import * as proxyquire from 'proxyquire';
 
 // Assuming GCPMetricsHandler is importable for type information,
 // but its constructor will be stubbed.
-import {GCPMetricsHandler as RealGCPMetricsHandler} from '../../src/client-side-metrics/gcp-metrics-handler';
 
 describe('ClientSideMetricsConfigManager', () => {
   let ClientSideMetricsConfigManager: any;
