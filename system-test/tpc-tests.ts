54186486917dd96933554e53323c12128126b748
Daniel Bruce <djbruce@google.com>
2025-05-14 10:38:21 -0400

    Make runTest async

--- system-test/tpc-tests.ts
+++ system-test/tpc-tests.ts
@@ -24,51 +24,45 @@ describe.skip('Universe domain tests', () => {
   // To see successful tests, uncomment the following line:
   // process.env.GOOGLE_APPLICATION_CREDENTIALS = '/path-to/key.json';
 
-  function runTest(done: Mocha.Done, bigtable: Bigtable) {
-    (async () => {
-      try {
-        const instance = bigtable.instance(instanceId);
-        const table = instance.table(tableId);
+  async function runTest(done: Mocha.Done, bigtable: Bigtable) {
+    try {
+      const instance = bigtable.instance(instanceId);
+      const table = instance.table(tableId);
 
-        // Create the instance if it doesn't exist:
-        const [instanceInfo] = await instance.exists();
-        if (!instanceInfo) {
-          const [, operation] = await instance.create({
-            clusters: {
-              id: 'fake-cluster3',
-              location: 'u-us-prp1-a',
-              nodes: 1,
-            },
-          });
-          await operation.promise();
-        }
-
-        // Create the table and the column families.
-        const [tableExists] = await table.exists();
-        if (!tableExists) {
-          await table.create({families: [columnFamilyId]}); // Create column family
-        } else {
-          // Check if column family exists and create it if not.
-          const [families] = await table.getFamilies();
-
-          if (
-            !families.some(
-              (family: {id: string}) => family.id === columnFamilyId,
-            )
-          ) {
-            await table.createFamily(columnFamilyId);
-          }
-        }
-
-        // Run the readrows operation:
-        await table.getRows();
-        done();
-      } catch (e) {
-        done(e);
+      // Create the instance if it doesn't exist:
+      const [instanceInfo] = await instance.exists();
+      if (!instanceInfo) {
+        const [, operation] = await instance.create({
+          clusters: {
+            id: 'fake-cluster3',
+            location: 'u-us-prp1-a',
+            nodes: 1,
+          },
+        });
+        await operation.promise();
       }
-    })().catch(err => {
-      throw err;
-    });
+
+      // Create the table and the column families.
+      const [tableExists] = await table.exists();
+      if (!tableExists) {
+        await table.create({families: [columnFamilyId]}); // Create column family
+      } else {
+        // Check if column family exists and create it if not.
+        const [families] = await table.getFamilies();
+
+        if (
+          !families.some((family: {id: string}) => family.id === columnFamilyId)
+        ) {
+          await table.createFamily(columnFamilyId);
+        }
+      }
+
+      // Run the readrows operation:
+      await table.getRows();
+      done();
+    } catch (e) {
+      done(e);
+    }
   }
 
   const instanceId = 'emulator-test-instance';
@@ -92,7 +86,9 @@ describe.skip('Universe domain tests', () => {
       universeDomain,
     };
     const bigtable = new Bigtable(options);
-    runTest(done, bigtable);
+    runTest(done, bigtable).catch(err => {
+      throw err;
+    });
   });
 
   it('should set the universe domain with a client option for the gapic clients', done => {
@@ -103,12 +99,16 @@ describe.skip('Universe domain tests', () => {
       BigtableTableAdminClient: {universeDomain},
     };
     const bigtable = new Bigtable(options);
-    runTest(done, bigtable);
+    runTest(done, bigtable).catch(err => {
+      throw err;
+    });
   });
 
   it('Should set universe domain with an environment variable', done => {
     process.env.GOOGLE_CLOUD_UNIVERSE_DOMAIN = UNIVERSE_DOMAIN_CONSTANT;
     const bigtable = new Bigtable();
-    runTest(done, bigtable);
+    runTest(done, bigtable).catch(err => {
+      throw err;
+    });
   });
 });
