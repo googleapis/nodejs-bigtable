7d54e3186e7ac2a71f1e3a3246185d8fce284b75
Kajetan Boroszko <kajetan@unoperate.com>
2025-06-12 14:52:00 +0200

    add sample

--- samples/api-reference-doc-snippets/instance.js
+++ samples/api-reference-doc-snippets/instance.js
@@ -369,7 +369,7 @@ const snippets = {
     const bigtable = new Bigtable();
     const instance = bigtable.instance(instanceId);
 
-    const query = `SELECT 
+    const query = `SELECT
                     _key
                   from \`${tableId}\` WHERE _key=@row_key`;
     const parameters = {
@@ -377,56 +377,64 @@ const snippets = {
     };
 
     // if query parameter types are ambiguous, you can pass types explicitly
-    const parameter_types = {
+    const parameterTypes = {
       row_key: Bigtable.ExecuteQueryTypes.String(),
     };
 
-    const options = {
+    const prepareStatementOptions = {
       query,
-      parameters,
-      parameter_types, // optional
+      parameterTypes,
     };
 
-    instance
-      .executeQuery(options)
-      .then(result => {
-        const rows = result[0];
+    instance.prepareStatement(prepareStatementOptions).then(
+      preparedStatement => instance.executeQuery({
+        preparedStatement,
+        parameters,
       })
-      .catch(err => {
-        // Handle the error.
-      });
+    ).then(result => {
+      const rows = result[0];
+    }).catch(err => {
+      // Handle errors
+    });
 
     // [END bigtable_api_execute_query]
   },
 
   createExecuteQueryStream: (instanceId, tableId) => {
     // [START bigtable_api_create_query_stream]
-    const {Bigtable} = require('@google-cloud/bigtable');
+    const { Bigtable } = require('@google-cloud/bigtable');
     const bigtable = new Bigtable();
     const instance = bigtable.instance(instanceId);
 
-    const query = `SELECT 
+    const query = `SELECT
                     _key
                   from \`${tableId}\` WHERE _key=@row_key`;
     const parameters = {
       row_key: 'alincoln',
     };
 
-    const options = {
+    const prepareStatementOptions = {
       query,
-      parameters,
+      parameterTypes,
     };
-    instance
-      .createExecuteQueryStream(options)
-      .on('error', err => {
-        // Handle the error.
-      })
-      .on('data', row => {
-        // `row` is a QueryResultRow object.
-      })
-      .on('end', () => {
-        // All rows retrieved.
-      });
+    instance.prepareStatement(prepareStatementOptions).then(
+      preparedStatement => {
+        instance
+          .createExecuteQueryStream({
+            preparedStatement,
+            parameters,
+          })
+          .on('error', err => {
+            // Handle the error.
+          })
+          .on('data', row => {
+            // `row` is a QueryResultRow object.
+          })
+          .on('end', () => {
+            // All rows retrieved.
+          });
+      }
+    )
 
     // If you anticipate many results, you can end a stream early to prevent
     // unnecessary processing.
