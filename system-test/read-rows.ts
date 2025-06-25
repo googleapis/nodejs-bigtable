b75c7eccaad3b524a65ce1e54b736126600d9d3f
Daniel Bruce <djbruce@google.com>
2025-06-25 11:33:24 -0400

    Remove the sinon clock

--- system-test/read-rows.ts
+++ system-test/read-rows.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {Bigtable} from '../src';
+import {AbortableDuplex, Bigtable} from '../src';
 import {Mutation} from '../src/mutation.js';
 const {tests} = require('../../system-test/data/read-rows-retry-test.json') as {
   tests: Test[];
@@ -25,6 +25,7 @@ import {EventEmitter} from 'events';
 import {Test} from './testTypes';
 import {ServiceError, GrpcClient, GoogleError, CallOptions} from 'google-gax';
 import {PassThrough} from 'stream';
+import * as mocha from 'mocha';
 
 const {grpc} = new GrpcClient();
 
@@ -57,7 +58,7 @@ function dispatch(emitter: EventEmitter, response: any) {
       const emit = emits[index];
       index++;
       emitter.emit(emit.name, emit.arg);
-      next();
+      setImmediate(next);
     }
   }
 }
@@ -124,33 +125,19 @@ describe('Bigtable/Table', () => {
   });
 
   describe.only('createReadStream', () => {
-    let clock: sinon.SinonFakeTimers;
     let endCalled: boolean;
     let error: ServiceError | null;
     let requestedOptions: Array<{}>;
     let responses: Array<{}> | null;
     let rowKeysRead: Array<Array<{}>>;
-    let stub: sinon.SinonStub;
 
     beforeEach(() => {
-      clock = sinon.useFakeTimers({
-        toFake: [
-          'setTimeout',
-          'clearTimeout',
-          'setImmediate',
-          'clearImmediate',
-          'setInterval',
-          'clearInterval',
-          'Date',
-          'nextTick',
-        ],
-      });
       endCalled = false;
       error = null;
       responses = null;
       rowKeysRead = [];
       requestedOptions = [];
-      stub = sinon.stub(bigtable, 'request').callsFake(cfg => {
+      bigtable.request = ((cfg: any) => {
         const reqOpts = cfg.reqOpts;
         const requestOptions = {} as google.bigtable.v2.IRowSet;
         if (reqOpts.rows && reqOpts.rows.rowRanges) {
@@ -182,39 +169,46 @@ describe('Bigtable/Table', () => {
         (requestStream as any).abort = () => {};
         dispatch(requestStream, responses!.shift());
         return requestStream;
-      });
-    });
-
-    afterEach(() => {
-      clock.restore();
-      stub.restore();
+      }) as unknown as () => AbortableDuplex;
     });
 
     tests.forEach(test => {
-      it(test.name, () => {
+      it(test.name, (done: mocha.Done) => {
         responses = test.responses;
         TABLE.maxRetries = test.max_retries;
         TABLE.createReadStream(test.createReadStream_options)
           .on('data', row => rowKeysRead[rowKeysRead.length - 1].push(row.id))
-          .on('end', () => (endCalled = true))
-          .on('error', err => (error = err as ServiceError));
-        clock.runAll();
-        console.log('clock');
+          .on('end', () => {
+            endCalled = true;
+            doAssertionChecks();
+          })
+          .on('error', err => {
+            error = err as ServiceError;
+            doAssertionChecks();
+          });
 
-        if (test.error) {
-          assert(!endCalled, ".on('end') should not have been invoked");
-          assert.strictEqual(error!.code, test.error);
-        } else {
-          assert(endCalled, ".on('end') shoud have been invoked");
-          assert.ifError(error);
+        function doAssertionChecks() {
+          try {
+            if (test.error) {
+              assert(!endCalled, ".on('end') should not have been invoked");
+              assert.strictEqual(error!.code, test.error);
+            } else {
+              assert(endCalled, ".on('end') shoud have been invoked");
+              assert.ifError(error);
+            }
+            assert.deepStrictEqual(rowKeysRead, test.row_keys_read);
+            assert(responses);
+            assert.strictEqual(
+              responses.length,
+              0,
+              'not all the responses were used',
+            );
+            assert.deepStrictEqual(requestedOptions, test.request_options);
+            done();
+          } catch (e) {
+            done(e);
+          }
         }
-        assert.deepStrictEqual(rowKeysRead, test.row_keys_read);
-        assert.strictEqual(
-          responses.length,
-          0,
-          'not all the responses were used',
-        );
-        assert.deepStrictEqual(requestedOptions, test.request_options);
       });
     });
   });
