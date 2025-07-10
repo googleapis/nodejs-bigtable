cdbdc20d0f75c43935c570857ff8ea4daef6690f
Daniel Bruce <djbruce@google.com>
2025-07-10 11:39:30 -0400

    Remove only

--- system-test/client-side-metrics-all-methods.ts
+++ system-test/client-side-metrics-all-methods.ts
@@ -260,7 +260,7 @@ async function checkForPublishedMetrics(projectId: string) {
   }
 }
 
-describe.only('Bigtable/ClientSideMetrics', () => {
+describe('Bigtable/ClientSideMetrics', () => {
   const instanceId1 = 'emulator-test-instance';
   const instanceId2 = 'emulator-test-instance2';
   const tableId1 = 'my-table';
