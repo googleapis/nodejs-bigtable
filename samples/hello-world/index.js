1ca6cf0329a84c0f5aca6061f03227c404acec5a
Daniel Bruce <djbruce@google.com>
2025-09-03 14:08:43 -0400

    Fix the index.js sample

--- samples/hello-world/index.js
+++ samples/hello-world/index.js
@@ -44,6 +44,7 @@ const getRowGreeting = row => {
     // [START bigtable_hw_connect]
     const bigtableClient = new Bigtable();
     const instance = bigtableClient.instance(INSTANCE_ID);
+    const table = instance.table(TABLE_ID);
     // [END bigtable_hw_connect]
 
     // [START bigtable_hw_create_table]
@@ -53,7 +54,9 @@ const getRowGreeting = row => {
 
     let tableExists = true;
     try {
-      await adminClient.getTable({name: adminClient.tablePath(projectId, INSTANCE_ID, TABLE_ID)});
+      await adminClient.getTable({
+        name: adminClient.tablePath(projectId, INSTANCE_ID, TABLE_ID),
+      });
     } catch (e) {
       if (e.code === 5) {
         tableExists = false;
@@ -137,9 +140,6 @@ const getRowGreeting = row => {
 
     // [START bigtable_hw_delete_table]
     console.log('Delete the table');
-    const {BigtableTableAdminClient} = require('@google-cloud/bigtable').v2;
-    const adminClient = new BigtableTableAdminClient();
-    const projectId = await adminClient.getProjectId();
     const request = {
       name: adminClient.tablePath(projectId, INSTANCE_ID, TABLE_ID),
     };
