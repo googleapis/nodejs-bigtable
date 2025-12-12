b350d5ea80e09401d25995e35c73561b32a10171
feywind <57276408+feywind@users.noreply.github.com>
2025-12-12 14:47:32 -0500

    feat: improve the type safety of GcRuleBuilder and add tests

--- src/admin/gc-rule-builder.ts
+++ src/admin/gc-rule-builder.ts
@@ -18,10 +18,21 @@ import {google} from '../../protos/protos';
 export type IGcRule = google.bigtable.admin.v2.IGcRule;
 export type IDuration = google.protobuf.IDuration;
 
+// Builds a type that has exactly one key from another type.
+type RequireExactlyOne<T, Keys extends keyof T = keyof T> = {
+  [K in Keys]-?: Required<Pick<T, K>> &
+    Partial<Record<Exclude<Keys, K>, never>>;
+}[Keys];
+
+// Builds a type from RequireExactlyOne that excludes certain keys.
+type BuildExclusiveType<T, ExcludedKeys extends keyof T> = RequireExactlyOne<
+  Omit<T, ExcludedKeys>
+>;
+
 export type GcUnion = Pick<IGcRule, 'union'>;
 export type GcIntersection = Pick<IGcRule, 'intersection'>;
 export type GcGrouping = GcUnion | GcIntersection;
-export type GcRuleItem = Omit<IGcRule, 'union' | 'intersection'>;
+export type GcRuleItem = BuildExclusiveType<IGcRule, 'union' | 'intersection'>;
 
 function isUnion(rule: IGcRule): rule is GcUnion {
   return !!rule.union;
@@ -35,8 +46,15 @@ function isGrouping(rule: IGcRule): rule is GcGrouping {
   return isUnion(rule) || isIntersection(rule);
 }
 
+function nonGroupKeys(rule: IGcRule): string[] {
+  return Object.keys(rule).filter(k => k !== 'union' && k !== 'intersection');
+}
+
 function isRuleItem(rule: IGcRule): rule is GcRuleItem {
-  return !isUnion(rule) && !isIntersection(rule);
+  const keys = nonGroupKeys(rule);
+
+  // We'll handle === 1 below.
+  return keys.length > 0;
 }
 
 /**
@@ -47,23 +65,44 @@ function isRuleItem(rule: IGcRule): rule is GcRuleItem {
  */
 export class GcRuleBuilder {
   /**
+   * Formats exception strings.
+   *
+   * @private
+   * @param p The IGcRule parameter that's failing
+   * @param why A string describing why it's failing
+   * @returns A formatted exception message
+   */
+  static formatErrorParameter(p: IGcRule, why: string): string {
+    return `Parameter ${util.format('%o', p)} ${why}`;
+  }
+
+  /**
    * Runtime checks for JavaScript code.
    * @private
    */
   static checkGroupingItem(p: GcGrouping | GcRuleItem): void {
-    if (!isGrouping(p) && !isRuleItem(p)) {
-      throw new Error(
-        `Parameter ${util.format('%o', p)} is not a union, intersection, or rule`,
-      );
-    }
     if (isUnion(p) && isIntersection(p)) {
       throw new Error(
-        `Parameter ${util.format('%o', p)} is both a union and intersection`,
+        GcRuleBuilder.formatErrorParameter(
+          p,
+          'is both a union and intersection',
+        ),
       );
     }
     if (isGrouping(p) && isRuleItem(p)) {
       throw new Error(
-        `Parameter ${util.format('%o', p)} is both a union and/or intersection, and a rule`,
+        GcRuleBuilder.formatErrorParameter(
+          p,
+          'is both a union and/or intersection, and a rule',
+        ),
+      );
+    }
+    if (!isGrouping(p) && !isRuleItem(p)) {
+      throw new Error(
+        GcRuleBuilder.formatErrorParameter(
+          p,
+          'does not appear to be a rule or a union/intersection',
+        ),
       );
     }
   }
@@ -73,9 +112,22 @@ export class GcRuleBuilder {
    * @private
    */
   static checkRuleItem(p: GcRuleItem): void {
+    if (isGrouping(p)) {
+      throw new Error(
+        GcRuleBuilder.formatErrorParameter(
+          p,
+          'is a union or intersection, not a rule',
+        ),
+      );
+    }
     if (!isRuleItem(p)) {
       throw new Error(
-        `Parameter ${util.format('%o', p)} is not a rule (or not only a rule)`,
+        GcRuleBuilder.formatErrorParameter(p, 'does not appear to be a rule'),
+      );
+    }
+    if (nonGroupKeys(p).length > 1) {
+      throw new Error(
+        GcRuleBuilder.formatErrorParameter(p, 'is an invalid multiple-rule'),
       );
     }
   }
@@ -88,12 +140,6 @@ export class GcRuleBuilder {
    * @returns [IGcRule] A newly built group
    */
   static union(...params: Array<GcGrouping | GcRuleItem>): GcGrouping {
-    if (params.length < 2) {
-      throw new Error(
-        `Unions should contain at least two items (passed: ${params.length})`,
-      );
-    }
-
     params.forEach(GcRuleBuilder.checkGroupingItem);
 
     return {
@@ -111,12 +157,6 @@ export class GcRuleBuilder {
    * @returns [IGcRule] A newly built group
    */
   static intersection(...params: Array<GcGrouping | GcRuleItem>): GcGrouping {
-    if (params.length < 2) {
-      throw new Error(
-        `Intersections should contain at least two items (passed: ${params.length})`,
-      );
-    }
-
     params.forEach(GcRuleBuilder.checkGroupingItem);
 
     return {
