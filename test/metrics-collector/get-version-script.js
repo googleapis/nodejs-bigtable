81c0ce4c91db9bb3f9ef289dfd895e3887a49a75
Daniel Bruce <djbruce@google.com>
2026-01-07 14:00:26 -0500

    Add a unit test that simulates customer code

---
+++ test/metrics-collector/get-version-script.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const fs = require('fs');
+const {
+  ClientSideMetricsConfigManager,
+} = require('../../build/src/client-side-metrics/metrics-config-manager.js');
+const {
+  TestMetricsHandlerKeepName,
+} = require('../../build/test-common/test-metrics-handler.js');
+const {Bigtable} = require('../../build/src/index.js');
+
+console.log('print version');
+
+async function main() {
+  const packagePath = path.join(__dirname, '../../package.json');
+
+  // Read the file using the absolute path
+  const packageJSON = fs.readFileSync(packagePath);
+  const expectedVersion = JSON.parse(packageJSON.toString()).version;
+
+  const fakeBigtable = new Bigtable();
+  const testMetricsHandler = new TestMetricsHandlerKeepName();
+  fakeBigtable._metricsConfigManager = new ClientSideMetricsConfigManager([
+    testMetricsHandler,
+  ]);
+
+  const instance = fakeBigtable.instance('fake-instance-id');
+  const table = instance.table('fake-table-id');
+  try {
+    await table.getRows();
+  } catch (e) {
+    // Suppress the error.
+    // We just made this call so that the test metrics handler would
+    // collect grpc response data.
+  }
+  if (
+    testMetricsHandler.requestsHandled[0].client_name !==
+    `nodejs-bigtable/${expectedVersion}`
+  ) {
+    console.log('Actual:');
+    console.log(testMetricsHandler.requestsHandled[0].client_name);
+    console.log('Expected:');
+    console.log(`nodejs-bigtable/${expectedVersion}`);
+    throw Error('The wrong version is being recorded');
+  }
+}
+main();
---
+++ test/metrics-collector/version.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line n/no-extraneous-import
+import {describe} from 'mocha';
+import {execSync} from 'node:child_process';
+
+describe.only('Bigtable/CSMVersion', () => {
+  it('Fetches the right client side metrics version', async () => {
+    execSync('cd test/metrics-collector && node get-version-script');
+  });
+});
