7a3b4d0cf98ff0ba71bcfb7965f32f3ce79724cd
Daniel Bruce <djbruce@google.com>
2026-01-07 14:12:39 -0500

    Remove the readSnippets script

--- samples/readSnippets.js
+++ samples/readSnippets.js
@@ -157,46 +157,6 @@ async function main(
       // [END bigtable_reads_filter]
       break;
     }
-
-    case 'readVersionMetrics': {
-      const path = require('path');
-      const fs = require('fs');
-      const {
-        ClientSideMetricsConfigManager,
-      } = require('./../build/src/client-side-metrics/metrics-config-manager');
-      const {
-        TestMetricsHandlerKeepName,
-      } = require('./../build/test-common/test-metrics-handler.js');
-
-      // [START bigtable_reads_version_metrics]
-      const packagePath = path.join(__dirname, '../package.json');
-
-      // Read the file using the absolute path
-      const packageJSON = fs.readFileSync(packagePath);
-      const expectedVersion = JSON.parse(packageJSON.toString()).version;
-
-      const fakeBigtable = new Bigtable();
-      const testMetricsHandler = new TestMetricsHandlerKeepName();
-      fakeBigtable._metricsConfigManager = new ClientSideMetricsConfigManager([
-        testMetricsHandler,
-      ]);
-
-      const instance = fakeBigtable.instance(instanceId);
-      const table = instance.table(tableId);
-      await table.getRows();
-      if (
-        testMetricsHandler.requestsHandled[0].client_name !==
-        `nodejs-bigtable/${expectedVersion}`
-      ) {
-        console.log('Actual:');
-        console.log(testMetricsHandler.requestsHandled[0].client_name);
-        console.log('Expected:');
-        console.log(`nodejs-bigtable/${expectedVersion}`);
-        throw Error('The wrong version is being recorded');
-      }
-      // [END bigtable_reads_version_metrics]
-      break;
-    }
   }
   // [END_EXCLUDE]
 
