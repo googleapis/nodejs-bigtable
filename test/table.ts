aade70e92bf4f9dc869b86b07be39b3bd44407fe
feywind <57276408+feywind@users.noreply.github.com>
2026-02-25 14:36:06 -0500

    chore: revert "tests: fix/bypass unit test issues caused by "tests: fix TestReadRow_Generic_DeadlineExceeded""
    This reverts commit 86c535ad5e5e3f8f41dcf67847cc32373729c812.

--- test/table.ts
+++ test/table.ts
@@ -1190,10 +1190,7 @@ describe('Bigtable/Table', () => {
           .on('data', done);
       });
     });
-
-    // Skip: This test currently doesn't make sense after conformance test
-    // changes. The error will always be DEADLINE_EXCEEDED, not UNAVAILABLE.
-    it.skip('Should respect the timeout parameter passed in for UNAVAILABLE error', done => {
+    it('Should respect the timeout parameter passed in for UNAVAILABLE error', done => {
       // The timeout is 2 seconds, but the error is received after 3 seconds
       // so the client doesn't retry because more than 2 seconds have elapsed.
       const requestSpy = (table.bigtable.request = sinon.spy(() => {
@@ -1220,7 +1217,6 @@ describe('Bigtable/Table', () => {
     it('Should respect the timeout parameter passed in for DEADLINE_EXCEEDED error', done => {
       // The timeout is 2 seconds, but the error is received after 3 seconds
       // so the client doesn't retry because more than 2 seconds have elapsed.
-      let timeoutReceived = false;
       const requestSpy = (table.bigtable.request = sinon.spy(() => {
         const stream = new PassThrough({
           objectMode: true,
@@ -1235,16 +1231,10 @@ describe('Bigtable/Table', () => {
       }));
       const stream = table.createReadStream({gaxOptions: {timeout: 2000}});
       stream.on('error', (error: ServiceError) => {
-        if (!timeoutReceived) {
-          assert.strictEqual(error.code, 4);
-          assert.strictEqual(
-            error.message,
-            'Total timeout of 2000ms exceeded.',
-          );
-          assert.strictEqual(requestSpy.callCount, 1); // Ensures the client has not retried.
-          done();
-        }
-        timeoutReceived = true;
+        assert.strictEqual(error.code, 4);
+        assert.strictEqual(error.message, 'retry me!');
+        assert.strictEqual(requestSpy.callCount, 1); // Ensures the client has not retried.
+        done();
       });
     });
     describe('retries', () => {
